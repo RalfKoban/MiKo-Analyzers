fc40cc8ccf20c5b2d7685b75d21b955ed890e454
Ralf Koban <rkn@mm-software.com>
2018-02-03 11:39:10 +0000

    LinesOfCodeAnalyzer updated, Closes #1

--- MiKo.Analyzer/Rules/Metrics/LinesOfCodeAnalyzer.cs
+++ MiKo.Analyzer/Rules/Metrics/LinesOfCodeAnalyzer.cs
@@ -1,7 +1,9 @@
 using System;
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
@@ -29,50 +31,76 @@ namespace MiKoSolutions.Analyzers.Rules.Metrics
         // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/Analyzer%20Actions%20Semantics.md for more information
         public override void Initialize(AnalysisContext context) => context.RegisterCodeBlockAction(AnalyzeCodeBlock);
 
-        private static LocalizableResourceString LocalizableResource(string suffix) => new LocalizableResourceString(nameof(LinesOfCodeAnalyzer) + suffix, Resources.ResourceManager, typeof(Resources));
+        private static LocalizableResourceString LocalizableResource(string suffix) => new LocalizableResourceString(nameof(LinesOfCodeAnalyzer) + "_" + suffix, Resources.ResourceManager, typeof(Resources));
 
         private void AnalyzeCodeBlock(CodeBlockAnalysisContext context)
         {
             var body = GetBody(context);
-            if (body != null)
-            {
-                AnalyzeBody(body, context);
-            }
+            if (body is null) return;
+
+            var diagnostic = AnalyzeBody(body, context.OwningSymbol);
+            if (diagnostic is null) return;
+
+            context.ReportDiagnostic(diagnostic);
         }
 
         private BlockSyntax GetBody(CodeBlockAnalysisContext context)
         {
             switch (context.CodeBlock)
             {
-                case MethodDeclarationSyntax m: return m.Body;
-                case ConstructorDeclarationSyntax c: return c.Body;
-                case AccessorDeclarationSyntax a: return a.Body;
+                case MethodDeclarationSyntax s: return s.Body;
+                case ConstructorDeclarationSyntax s: return s.Body;
+                case AccessorDeclarationSyntax s: return s.Body;
                 default: return null;
             }
         }
 
-        static readonly string[] Separators = { Environment.NewLine };
-        static readonly char[] SeparatorChars = Environment.NewLine.ToCharArray();
-
-        private void AnalyzeBody(BlockSyntax body, CodeBlockAnalysisContext context)
+        private Diagnostic AnalyzeBody(BlockSyntax body, ISymbol owningSymbol)
         {
-            var sourceText = body.WithoutTrivia().GetText();
+            var loc = CountLinesOfCode(body);
 
-            // TODO: RKN Fix performance issue of creating new objects
-            var lines = sourceText.ToString().Trim(SeparatorChars).Split(Separators, StringSplitOptions.None);
-            var loc = lines.Length;
-            if (loc > 0)
+            return loc > MaxLinesOfCode
+                    ? Diagnostic.Create(Rule, owningSymbol.Locations.First(), owningSymbol.Name, loc, MaxLinesOfCode)
+                    : null;
+        }
+
+        private static int CountLinesOfCode(SyntaxNode body)
+        {
+            var collector = new SyntaxNodeCollector<StatementSyntax>();
+            collector.Visit(body);
+
+            var lines = new HashSet<int>();
+
+            foreach (var node in collector.Nodes)
             {
-                var firstLine = lines[0];
-                var lastLine = lines.Last();
-                if (firstLine.Contains('{') && string.IsNullOrWhiteSpace(firstLine.Replace('{', ' '))) loc--;
-                if (lastLine.Contains('}') && string.IsNullOrWhiteSpace(lastLine.Replace('}', ' '))) loc--;
+                CountLinesOfCode(node, lines);
             }
 
-            if (loc > MaxLinesOfCode)
+            return lines.Count;
+        }
+
+        private static void CountLinesOfCode(SyntaxNode node, ISet<int> lines)
+        {
+            switch (node)
             {
-                context.ReportDiagnostic(Diagnostic.Create(Rule, context.OwningSymbol.Locations.First(), context.OwningSymbol.Name, loc, MaxLinesOfCode));
+                case BlockSyntax _:
+                    break;
+
+                case ForEachStatementSyntax s:
+                    CountLinesOfCode(s.Expression, lines);
+                    break;
+
+                case SwitchStatementSyntax s:
+                    CountLinesOfCode(s.Expression, lines);
+                    break;
+
+                default:
+                    var lineSpan = node.GetLocation().GetLineSpan();
+                    lines.Add(lineSpan.StartLinePosition.Line);
+                    lines.Add(lineSpan.EndLinePosition.Line);
+                    break;
             }
+
         }
     }
 }
