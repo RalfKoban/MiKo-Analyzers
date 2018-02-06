bbf7475421fb5be1d3d87a606ce52c329c07183e
Ralf Koban <rkn@mm-software.com>
2018-02-06 14:57:50 +0000

    More tests for #4

--- MiKo.Analyzer.Tests/Rules/Documentation/SealedClassAnalyzerTests.cs
+++ MiKo.Analyzer.Tests/Rules/Documentation/SealedClassAnalyzerTests.cs
@@ -10,11 +10,74 @@ namespace MiKoSolutions.Analyzers.Rules.Documentation
     public class SealedClassAnalyzerTests : CodeFixVerifier
     {
         [Test]
+        public void Unsealed_class_is_not_reported()
+        {
+            No_issue_gets_reported(@"
+/// <summary>
+/// Something.
+/// </summary>
+public class TestMe
+{
+}
+");
+        }
+
+        [Test]
+        public void Struct_is_not_reported()
+        {
+            No_issue_gets_reported(@"
+/// <summary>
+/// Something.
+/// </summary>
+public struct TestMe
+{
+}
+");
+        }
+
+        [Test]
+        public void No_documentation_gets_not_reported()
+        {
+            No_issue_gets_reported(@"
+public sealed class TestMe
+{
+}
+");
+        }
+
+        [Test]
         public void Correct_documentation_gets_not_reported()
         {
             No_issue_gets_reported(@"
 /// <summary>
 /// This class cannot be inherited.
+/// </summary>
+public sealed class TestMe
+{
+}
+");
+        }
+
+        [Test]
+        public void Missing_documentation_gets_reported()
+        {
+            Issue_gets_reported(@"
+/// <summary>
+/// Some documentation
+/// </summary>
+public sealed class TestMe
+{
+}
+");
+        }
+
+        [Test]
+        public void Wrong_placed_documentation_gets_reported()
+        {
+            Issue_gets_reported(@"
+/// <summary>
+/// This class cannot be inherited.
+/// Some documentation
 /// </summary>
 public sealed class TestMe
 {
