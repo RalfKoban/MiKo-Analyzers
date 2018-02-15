6523b9e739d28631bfa32849e505f6704d53d69b
Ralf Koban <rkn@mm-software.com>
2018-02-15 16:52:50 +0000

    some more suffixed added

--- MiKo.Analyzer/Rules/Naming/NamingAnalyzer.cs
+++ MiKo.Analyzer/Rules/Naming/NamingAnalyzer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 
 using Microsoft.CodeAnalysis;
 
@@ -6,15 +7,32 @@ namespace MiKoSolutions.Analyzers.Rules.Naming
 {
     public abstract class NamingAnalyzer : Analyzer
     {
+        private static readonly ConcurrentDictionary<string, string> PluralNames = new ConcurrentDictionary<string, string>();
+
         protected NamingAnalyzer(string diagnosticId, SymbolKind kind = SymbolKind.Method, bool isEnabledByDefault = true) : base(nameof(Naming), diagnosticId, kind, isEnabledByDefault)
         {
         }
 
-        protected static string GetPluralName(string symbolName, string suffix, StringComparison comparison)
+        protected Diagnostic AnalyzeCollectionSuffix(ISymbol symbol) => AnalyzeSuffix(symbol, "List") ?? AnalyzeSuffix(symbol, "Dictionary") ?? AnalyzeSuffix(symbol, "Collection");
+
+        protected Diagnostic AnalyzeSuffix(ISymbol symbol, string suffix, StringComparison comparison = StringComparison.Ordinal)
+        {
+            var symbolName = symbol.Name;
+            if (!symbolName.EndsWith(suffix, comparison)) return null;
+            if (symbolName == "blackList" || symbolName == "whiteList") return null;
+
+            var betterName = PluralNames.GetOrAdd(symbolName, _ => GetPluralName(symbolName, suffix, comparison));
+            return ReportIssue(symbol, betterName);
+        }
+
+        private static string GetPluralName(string symbolName, string suffix, StringComparison comparison)
         {
             var name = symbolName.Substring(0, symbolName.Length - suffix.Length);
             if (name.EndsWith("y", comparison)) return name.Substring(0, name.Length - 1) + "ies";
             if (name.EndsWith("ss", comparison)) return name + "es";
+            if (name.EndsWith("ed", comparison)) return name;
+            if (name.EndsWith("child", comparison)) return name + "ren";
+            if (name.EndsWith("children", comparison)) return name;
             if (name.EndsWith("complete", comparison)) return "all";
             if (name.EndsWith("Data", comparison)) return name;
             if (name.EndsWith("Datas", comparison)) return name.Substring(0, name.Length - 1);
@@ -29,16 +47,5 @@ namespace MiKoSolutions.Analyzers.Rules.Naming
 
             return pluralName.EndsWith("s", comparison) ? pluralName : pluralName + "s";
         }
-
-        protected Diagnostic AnalyzeSuffix(ISymbol symbol, string suffix, StringComparison comparison = StringComparison.Ordinal)
-        {
-            var symbolName = symbol.Name;
-            if (!symbolName.EndsWith(suffix, comparison)) return null;
-
-            var betterName = GetPluralName(symbolName, suffix, comparison);
-            return ReportIssue(symbol, betterName);
-        }
-
-        protected Diagnostic AnalyzeCollectionSuffix(ISymbol symbol) => AnalyzeSuffix(symbol, "List") ?? AnalyzeSuffix(symbol, "Dictionary") ?? AnalyzeSuffix(symbol, "Collection");
     }
 }
