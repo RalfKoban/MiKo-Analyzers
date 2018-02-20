1dd18d6faac86071bf44c2b18a9cba15b4c18fc1
Ralf Koban <rkn@mm-software.com>
2018-02-20 09:56:04 +0000

    Refactoring for #62

--- MiKo.Analyzer/Rules/Naming/NamingAnalyzer.cs
+++ MiKo.Analyzer/Rules/Naming/NamingAnalyzer.cs
@@ -25,45 +25,45 @@ namespace MiKoSolutions.Analyzers.Rules.Naming
             var symbolName = symbol.Name;
             if (!symbolName.EndsWith(suffix, comparison)) return null;
 
-            switch (symbolName)
+            if (symbolName == "blackList" || symbolName == "whiteList")
             {
-                case "blackList":
-                case "whiteList":
-                    return null;
-                default:
-                    {
-                        var betterName = PluralNames.GetOrAdd(symbolName, _ => GetPluralName(symbolName, suffix, comparison));
-                        return ReportIssue(symbol, betterName);
-                    }
+                return null;
             }
+
+            var length = symbolName.Length - suffix.Length;
+            if (length <= 0) return null; // use complete symbol name
+
+            var proposedName = symbolName.Substring(0, length);
+            if (symbolName.IsEntityMarker())
+                proposedName = proposedName.Substring(0, proposedName.Length - 5);
+
+            var betterName = PluralNames.GetOrAdd(symbolName, _ => GetPluralName(symbolName, proposedName, comparison));
+            return ReportIssue(symbol, betterName);
         }
 
-        private static string GetPluralName(string symbolName, string suffix, StringComparison comparison)
+        private static string GetPluralName(string symbolName, string proposedName, StringComparison comparison)
         {
-            var length = symbolName.Length - suffix.Length;
-            if (length <= 0) return symbolName; // use complete symbol name
+            if (proposedName.EndsWith("y", comparison)) return proposedName.Substring(0, proposedName.Length - 1) + "ies";
+            if (proposedName.EndsWith("ss", comparison)) return proposedName + "es";
+            if (proposedName.EndsWith("ed", comparison)) return proposedName;
+            if (proposedName.EndsWith("child", comparison)) return proposedName + "ren";
+            if (proposedName.EndsWith("children", comparison)) return proposedName;
+            if (proposedName.EndsWith("complete", comparison)) return "all";
+            if (proposedName.EndsWith("Data", comparison)) return proposedName;
+            if (proposedName.EndsWith("Datas", comparison)) return proposedName.Substring(0, proposedName.Length - 1);
+            if (proposedName.EndsWith("ndex", comparison)) return proposedName.Substring(0, proposedName.Length - 2) + "ices";
+            if (proposedName.EndsWith("nformation", comparison)) return proposedName;
+            if (proposedName.EndsWith("nformations", comparison)) return proposedName.Substring(0, proposedName.Length - 1);
 
-            var name = symbolName.Substring(0, length);
-            if (name.EndsWith("y", comparison)) return name.Substring(0, name.Length - 1) + "ies";
-            if (name.EndsWith("ss", comparison)) return name + "es";
-            if (name.EndsWith("ed", comparison)) return name;
-            if (name.EndsWith("child", comparison)) return name + "ren";
-            if (name.EndsWith("children", comparison)) return name;
-            if (name.EndsWith("complete", comparison)) return "all";
-            if (name.EndsWith("Data", comparison)) return name;
-            if (name.EndsWith("Datas", comparison)) return name.Substring(0, name.Length - 1);
-            if (name.EndsWith("ndex", comparison)) return name.Substring(0, name.Length - 2) + "ices";
-            if (name.EndsWith("nformation", comparison)) return name;
-            if (name.EndsWith("nformations", comparison)) return name.Substring(0, name.Length - 1);
-
-            var pluralName = name;
-            if (symbolName.IsEntityMarker())
-                pluralName = name.Substring(0, name.Length - 5);
-            else if (name.EndsWith("ToConvert", comparison))
-                pluralName = name.Substring(0, name.Length - "ToConvert".Length);
+            var pluralName = proposedName;
+            if (proposedName.EndsWith("ToConvert", comparison))
+                pluralName = proposedName.Substring(0, proposedName.Length - "ToConvert".Length);
 
             var candidate = pluralName.EndsWith("s", comparison) ? pluralName : pluralName + "s";
+
             if (candidate.Equals("sources", comparison)) return "source"; // special handling
+            if (candidate.Equals("_sources", comparison)) return "_source"; // special handling
+
             return candidate;
         }
     }
