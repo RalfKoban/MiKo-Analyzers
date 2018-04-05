b617995b50a613f35574b2399cf6f6fdbee90667
Ralf Koban <rkn@mm-software.com>
2018-04-05 10:02:45 +0000

    Resolves #29

---
+++ MiKo.Analyzer.Tests/Rules/Documentation/MiKo_2015_FireMethodsAnalyzerTests.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.CodeAnalysis.Diagnostics;
+
+using NUnit.Framework;
+
+using TestHelper;
+
+namespace MiKoSolutions.Analyzers.Rules.Documentation
+{
+    [TestFixture]
+    public sealed class MiKo_2015_FireMethodsAnalyzerTests : CodeFixVerifier
+    {
+        [Test]
+        public void No_issue_is_reported_for_undocumented_items() => No_issue_is_reported_for(@"
+using System;
+
+public class TestMe
+{
+    public event EventHandler<T> MyEvent;
+
+    public void DoSomething() { }
+
+    public int Age { get; set; }
+
+    private bool m_field;
+}
+
+");
+
+        [Test]
+        public void No_issue_is_reported_for_correctly_documented_items() => No_issue_is_reported_for(@"
+using System;
+
+/// <summary>Does something.</summary>
+/// <remarks>Does something.</remarks>
+public class TestMe
+{
+    /// <summary>Does something.</summary>
+    /// <remarks>Does something.</remarks>
+    public event EventHandler<T> MyEvent;
+
+    /// <summary>Does something.</summary>
+    /// <remarks>Does something.</remarks>
+    public void DoSomething() { }
+
+    /// <summary>Does something.</summary>
+    /// <remarks>Does something.</remarks>
+    public int Age { get; set; }
+
+    /// <summary>Does something.</summary>
+    /// <remarks>Does something.</remarks>
+    private bool m_field;
+}
+");
+
+        [Test]
+        public void An_issue_is_reported_for_correctly_documented_summary_on_class([ValueSource(nameof(XmlTags))] string tag) => An_issue_is_reported_for(@"
+using System;
+
+/// <" + tag + @">Does fire.</" + tag + @">
+public class TestMe
+{
+}
+");
+
+        [Test]
+        public void An_issue_is_reported_for_correctly_documented_summary_on_method([ValueSource(nameof(XmlTags))] string tag) => An_issue_is_reported_for(@"
+using System;
+
+public class TestMe
+{
+    /// <" + tag + @">Does fire.</" + tag + @">
+    public void DoSomething() { }
+}
+");
+
+        [Test]
+        public void An_issue_is_reported_for_correctly_documented_summary_on_property([ValueSource(nameof(XmlTags))] string tag) => An_issue_is_reported_for(@"
+using System;
+
+public class TestMe
+{
+    /// <" + tag + @">Does fire.</" + tag + @">
+    public int Age { get; set; }
+}
+");
+
+        [Test]
+        public void An_issue_is_reported_for_correctly_documented_summary_on_event([ValueSource(nameof(XmlTags))] string tag) => An_issue_is_reported_for(@"
+using System;
+
+public class TestMe
+{
+    /// <" + tag + @">Does fire.</" + tag + @">
+    public event EventHandler<T> MyEvent;
+}
+");
+
+        [Test]
+        public void An_issue_is_reported_for_correctly_documented_summary_on_field([ValueSource(nameof(XmlTags))] string tag) => An_issue_is_reported_for(@"
+using System;
+
+public class TestMe
+{
+    /// <" + tag + @">Does fire.</" + tag + @">
+    private bool m_field;
+}
+");
+
+        protected override string GetDiagnosticId() => MiKo_2015_FireMethodsAnalyzer.Id;
+
+        protected override DiagnosticAnalyzer GetObjectUnderTest() => new MiKo_2015_FireMethodsAnalyzer();
+
+        private static IEnumerable<string> XmlTags() => new[] { "summary", "remarks", "returns", "example" };
+    }
+}
