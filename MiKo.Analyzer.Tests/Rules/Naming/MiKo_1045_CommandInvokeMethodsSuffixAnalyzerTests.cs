50ddd050ba4dafd520333c2977f872a8b5551385
Ralf Koban <rkn@mm-software.com>
2018-04-13 05:44:22 +0000

    Resolves #65

---
+++ MiKo.Analyzer.Tests/Rules/Naming/MiKo_1045_CommandInvokeMethodsSuffixAnalyzerTests.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+
+using NUnit.Framework;
+
+using TestHelper;
+
+namespace MiKoSolutions.Analyzers.Rules.Naming
+{
+    [TestFixture]
+    public sealed class MiKo_1045_CommandInvokeMethodsSuffixAnalyzerTests : CodeFixVerifier
+    {
+        [Test]
+        public void No_issue_is_reported_for_non_command_type() => No_issue_is_reported_for(@"
+using System;
+
+public class TestMe
+{
+    public object DoSomething()
+    {
+        return new object();
+    }
+}
+");
+
+        [Test]
+        public void No_issue_is_reported_for_correctly_named_command_methods() => No_issue_is_reported_for(@"
+using System;
+using System.Windows.Input;
+
+public class TestMeCommand : ICommand
+{
+    public TestMeCommand(Action execute) => _execute = execute;
+
+    public event EventHandler CanExecuteChanged;
+
+    public bool CanExecute(object parameter) => true;
+
+    public void Execute(object parameter) _execute();
+
+    private Action _execute;
+}
+
+public class TestMe
+{
+    public void DoSomething()
+    {
+        var testMeCommand = new TestMeCommand(DoSomething);
+    }
+}
+");
+
+        [Test]
+        public void An_issue_is_reported_for_incorrectly_named_command_methods() => An_issue_is_reported_for(@"
+using System;
+using System.Windows.Input;
+
+public class TestMeCommand : ICommand
+{
+    public TestMeCommand(Action execute) => _execute = execute;
+
+    public event EventHandler CanExecuteChanged;
+
+    public bool CanExecute(object parameter) => true;
+
+    public void Execute(object parameter) _execute();
+
+    private Action _execute;
+}
+
+public class TestMe
+{
+    public void DoSomething()
+    {
+        var testMeCommand = new TestMeCommand(TestCommand);
+    }
+
+    private void TestCommand() { }
+}
+");
+
+        protected override string GetDiagnosticId() => MiKo_1045_CommandInvokeMethodsSuffixAnalyzer.Id;
+
+        protected override DiagnosticAnalyzer GetObjectUnderTest() => new MiKo_1045_CommandInvokeMethodsSuffixAnalyzer();
+    }
+}
---
+++ MiKo.Analyzer/Rules/Naming/MiKo_1045_CommandInvokeMethodsSuffixAnalyzer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace MiKoSolutions.Analyzers.Rules.Naming
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class MiKo_1045_CommandInvokeMethodsSuffixAnalyzer : NamingAnalyzer
+    {
+        public const string Id = "MiKo_1045";
+
+        private const string Suffix = "Command";
+
+        public MiKo_1045_CommandInvokeMethodsSuffixAnalyzer() : base(Id, (SymbolKind)(-1))
+        {
+        }
+
+        protected override void InitializeCore(AnalysisContext context) => context.RegisterSyntaxNodeAction(AnalyzeObjectCreation, SyntaxKind.ObjectCreationExpression);
+
+        private void AnalyzeObjectCreation(SyntaxNodeAnalysisContext context)
+        {
+            var node = (ObjectCreationExpressionSyntax)context.Node;
+            var type = node.Type.ToString();
+            if (!type.Contains(Suffix)) return;
+
+            var diagnostics = AnalyzeObjectCreation(node);
+            foreach (var diagnostic in diagnostics)
+            {
+                context.ReportDiagnostic(diagnostic);
+            }
+        }
+
+        private IEnumerable<Diagnostic> AnalyzeObjectCreation(ObjectCreationExpressionSyntax node)
+        {
+            var arguments = node.ArgumentList.Arguments;
+            return arguments.Any()
+                       ? arguments.Where(_ => _.ToString().EndsWith(Suffix, StringComparison.Ordinal))
+                                  .Select(_ => ReportIssue(_.ToString(), _.GetLocation(), Suffix))
+                                  .ToList()
+                       : Enumerable.Empty<Diagnostic>();
+        }
+    }
+}
