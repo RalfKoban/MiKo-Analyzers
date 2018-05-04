51833b127b3a4d29dd8dbf129708c6c267f47494
Ralf Koban <rkn@mm-software.com>
2018-05-04 07:40:30 +0000

    Bugfix for #80 (now, bool type gets underlined)

--- MiKo.Analyzer/Rules/Maintainability/MiKo_3040_BooleanMethodParametersAnalyzer.cs
+++ MiKo.Analyzer/Rules/Maintainability/MiKo_3040_BooleanMethodParametersAnalyzer.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace MiKoSolutions.Analyzers.Rules.Maintainability
@@ -16,7 +17,12 @@ namespace MiKoSolutions.Analyzers.Rules.Maintainability
         }
 
         protected override IEnumerable<Diagnostic> AnalyzeMethod(IMethodSymbol method) => method.MethodKind == MethodKind.Ordinary
-                                                                                              ? method.Parameters.Where(_ => _.Type.SpecialType == SpecialType.System_Boolean).Select(_ => ReportIssue(_))
+                                                                                              ? method.Parameters
+                                                                                                      .Where(_ => _.Type.SpecialType == SpecialType.System_Boolean)
+                                                                                                      .SelectMany(_ => _.DeclaringSyntaxReferences)
+                                                                                                      .Select(_ => _.GetSyntax())
+                                                                                                      .OfType<ParameterSyntax>()
+                                                                                                      .Select(_ => ReportIssue(_.Identifier.ValueText, _.Type.GetLocation()))
                                                                                               : Enumerable.Empty<Diagnostic>();
     }
 }
