cea49aa18bac5e53a232d8e713c446a7894359b7
Ralf Koban <rkn@mm-software.com>
2018-12-10 12:10:03 +0000

    MiKo_1400 now also ignores namespace that end with 'ren' (such as in 'Children')

--- MiKo.Analyzer/Rules/Naming/MiKo_1400_NamespacesInPluralAnalyzer.cs
+++ MiKo.Analyzer/Rules/Naming/MiKo_1400_NamespacesInPluralAnalyzer.cs
@@ -16,6 +16,7 @@ namespace MiKoSolutions.Analyzers.Rules.Naming
             {
                 "s",
                 "ing",
+                "ren",
                 "ComponentModel",
                 "Composition",
                 "Data",
