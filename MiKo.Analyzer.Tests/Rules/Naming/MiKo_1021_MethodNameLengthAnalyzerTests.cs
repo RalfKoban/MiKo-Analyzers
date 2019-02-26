5941f46e277f7a6029b2c73305e6da4d7f8f95c2
Ralf Koban <rkn@mm-software.com>
2019-02-26 12:42:38 +0000

    test fixed

--- MiKo.Analyzer.Tests/Rules/Naming/MiKo_1021_MethodNameLengthAnalyzerTests.cs
+++ MiKo.Analyzer.Tests/Rules/Naming/MiKo_1021_MethodNameLengthAnalyzerTests.cs
@@ -27,7 +27,7 @@ public void " + name + @"()
 ");
 
         [Test]
-        public void No_issue_is_reported_for_test_method_with_exceeding_length([ValueSource(nameof(Tests))] string name) => No_issue_is_reported_for(@"
+        public void No_issue_is_reported_for_test_method_with_exceeding_length([ValueSource(nameof(TestsExceptSetUpTearDowns))] string name) => No_issue_is_reported_for(@"
 
 [" + name + @"]
 public void Abcdefghijklmnopqrstuvwxyz()
