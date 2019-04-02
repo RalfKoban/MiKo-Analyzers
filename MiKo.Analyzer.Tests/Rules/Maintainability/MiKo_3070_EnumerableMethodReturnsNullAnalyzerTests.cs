27d822f2ce2cd1d016dd381d1db1a11b871bc42d
Ralf Koban <rkn@mm-software.com>
2019-04-02 16:19:31 +0000

    current development state for #156

---
+++ MiKo.Analyzer.Tests/Rules/Maintainability/MiKo_3070_EnumerableMethodReturnsNullAnalyzerTests.cs
@@ -0,0 +1,137 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+
+using NUnit.Framework;
+
+using TestHelper;
+
+namespace MiKoSolutions.Analyzers.Rules.Maintainability
+{
+    [TestFixture]
+    public sealed class MiKo_3070_EnumerableMethodReturnsNullAnalyzerTests : CodeFixVerifier
+    {
+        [Test]
+        public void No_issue_reported_for_void_method() => No_issue_is_reported_for(@"
+public class TestMe
+{
+    public void DoSomething() {}
+}");
+
+        [Test]
+        public void No_issue_reported_for_non_Enumerable_method() => No_issue_is_reported_for(@"
+public class TestMe
+{
+    public object DoSomething()
+    {
+        return null;
+    }
+}");
+
+        [Test]
+        public void No_issue_reported_for_non_Enumerable_method_body() => No_issue_is_reported_for(@"
+public class TestMe
+{
+    public object DoSomething() => null;
+}");
+
+        [Test]
+        public void No_issue_reported_for_Enumerable_method_returning_a_valid_List() => No_issue_is_reported_for(@"
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public IEnumerable DoSomething()
+        {
+            return new List<int>();
+        }
+    }
+}");
+
+        [Test]
+        public void No_issue_reported_for_Enumerable_method_body_returning_a_valid_List() => No_issue_is_reported_for(@"
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public IEnumerable DoSomething() => new List<int>();
+    }
+}");
+
+        [Test]
+        public void An_issue_reported_for_Enumerable_method_returning_null() => An_issue_is_reported_for(@"
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public IEnumerable DoSomething()
+        {
+            return null;
+        }
+    }
+}");
+
+        [Test]
+        public void An_issue_reported_for_Enumerable_method_body_returning_null() => An_issue_is_reported_for(@"
+using System.Collections;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public IEnumerable DoSomething() => null;
+    }
+}");
+
+        [Test]
+        public void An_issue_reported_for_Enumerable_method_returning_a_variable_that_is_null() => An_issue_is_reported_for(@"
+using System.Collections;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public IEnumerable DoSomething()
+        {
+            IEnumerable variable = null;
+            return variable;
+        }
+    }
+}");
+
+        [Test]
+        public void An_issue_reported_for_Enumerable_method_returning_a_variable_that_is_potentially_null() => An_issue_is_reported_for(@"
+using System.Collections;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public IEnumerable DoSomething(bool flag)
+        {
+            IEnumerable variable;
+
+            if (flag)
+                variable = new List<int>()
+            else
+                variable = null;
+
+            return variable;
+        }
+    }
+}");
+
+        // TODO: RKN what about Linq calls such as FirstOrDefault();
+
+        protected override string GetDiagnosticId() => MiKo_3070_EnumerableMethodReturnsNullAnalyzer.Id;
+
+        protected override DiagnosticAnalyzer GetObjectUnderTest() => new MiKo_3070_EnumerableMethodReturnsNullAnalyzer();
+    }
+}
---
+++ MiKo.Analyzer/Rules/Maintainability/MiKo_3070_EnumerableMethodReturnsNullAnalyzer.cs
@@ -0,0 +1,96 @@
+﻿
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace MiKoSolutions.Analyzers.Rules.Maintainability
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class MiKo_3070_EnumerableMethodReturnsNullAnalyzer : MaintainabilityAnalyzer
+    {
+        public const string Id = "MiKo_3070";
+
+        public MiKo_3070_EnumerableMethodReturnsNullAnalyzer() : base(Id)
+        {
+        }
+
+        protected override void InitializeCore(AnalysisContext context)
+        {
+            context.RegisterSyntaxNodeAction(AnalyzeMethod, SyntaxKind.MethodDeclaration);
+        }
+
+        private static bool CanBeIgnored(SyntaxNodeAnalysisContext context) => CanBeIgnored(context.GetEnclosingMethod());
+
+        private static bool CanBeIgnored(IMethodSymbol method)
+        {
+            switch (method?.ReturnType.SpecialType)
+            {
+                case SpecialType.System_Collections_IEnumerable:
+                case SpecialType.System_Collections_Generic_IEnumerable_T:
+                    return false;
+
+                default:
+                    return true;
+            }
+        }
+
+        private void AnalyzeMethod(SyntaxNodeAnalysisContext context)
+        {
+            var method = (MethodDeclarationSyntax)context.Node;
+            var methodBody = method.Body ?? (SyntaxNode)method.ExpressionBody?.Expression;
+
+            Analyze(context, methodBody);
+        }
+
+        private void Analyze(SyntaxNodeAnalysisContext context, SyntaxNode methodBody)
+        {
+            if (methodBody is null)
+                return;
+
+            if (CanBeIgnored(context))
+                return;
+
+            if (methodBody is ObjectCreationExpressionSyntax)
+                return;
+
+            var semanticModel = context.SemanticModel;
+
+            if (methodBody.IsNullExpression(semanticModel))
+            {
+                ReportIssue(context, methodBody);
+                return;
+            }
+
+//            var assignments = methodBody.DescendantNodes().OfType<EqualsValueClauseSyntax>().ToList();
+
+            var controlFlow = semanticModel.AnalyzeControlFlow(methodBody);
+            var returnStatements = controlFlow.ReturnStatements.OfType<ReturnStatementSyntax>();
+            foreach (var returnStatement in returnStatements)
+            {
+                if (returnStatement.Expression.IsNullExpression(semanticModel))
+                {
+                    ReportIssue(context, returnStatement);
+                }
+                else
+                {
+                    var dataFlow = semanticModel.AnalyzeDataFlow(returnStatement);
+                    foreach (var localVariable in dataFlow.ReadInside)
+                    {
+
+                        // TODO: RKN get assignments to variable
+
+                    }
+                }
+            }
+        }
+
+        private void ReportIssue(SyntaxNodeAnalysisContext context, SyntaxNode node)
+        {
+            var name = node.ToString();
+            var diagnostic = ReportIssue(name, node.GetLocation());
+
+            context.ReportDiagnostic(diagnostic);
+        }
+    }
+}
