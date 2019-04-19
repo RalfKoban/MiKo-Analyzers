8bd3cdef836f93207c788438d9c4c927de032e49
Ralf Koban <rkn@mm-software.com>
2019-04-19 15:33:34 +0000

    Part for issue #133

---
+++ MiKo.Analyzer.Tests/Rules/Naming/MiKo_1063_AbbreviationsInNameAnalyzerTests.cs
@@ -0,0 +1,154 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+
+using NUnit.Framework;
+
+using TestHelper;
+
+namespace MiKoSolutions.Analyzers.Rules.Naming
+{
+    [TestFixture]
+    public sealed class MiKo_1063_AbbreviationsInNameAnalyzerTests : CodeFixVerifier
+    {
+        private static readonly string[] BadPrefixes = { "txt" };
+
+        // - `cb` for `checkBox`
+        // - `lbl` for `label` 
+        // - `txt` for `text`
+        // - `Cmd` for `Command`
+        // - `Mgr` for `Manager`
+        // - `BL` or `Bl` for `BusinessLogic` >> should be never used
+        // - `VM` for `ViewModel`
+        // - `tmp` for `temp`
+        // - `Prop` for `Property`
+        // - `PS` or `Ps` for `ProjectStructure` >> should be never used
+        // - `Msg` for `Message`
+
+        [Test]
+        public void No_issue_is_reported_for_properly_named_code() => No_issue_is_reported_for(@"
+using System;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        private int m_i;
+
+        public TestMe(int i)
+        {
+            m_i = i;
+        }
+
+        public event EventHandler Raised;
+
+        public string Name { get; set; }
+
+        public int DoSomething()
+        {
+            var x = 42;
+            return x;
+        }
+    }
+}
+");
+
+        [Test]
+        public void An_issue_is_reported_for_local_variable_with_prefix([ValueSource(nameof(BadPrefixes))] string prefix) => An_issue_is_reported_for(@"
+using System;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public int DoSomething()
+        {
+            var " + prefix + @"Variable = 42;
+            return " + prefix + @"Variable;
+        }
+    }
+}");
+
+        [Test]
+        public void An_issue_is_reported_for_field_with_prefix([ValueSource(nameof(BadPrefixes))] string prefix) => An_issue_is_reported_for(@"
+using System;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        private int " + prefix + @"Field = 42;
+    }
+}");
+
+        [Test]
+        public void An_issue_is_reported_for_property_with_prefix([ValueSource(nameof(BadPrefixes))] string prefix) => An_issue_is_reported_for(@"
+using System;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public int " + prefix + @"Property { get; set; }
+    }
+}");
+
+        [Test]
+        public void An_issue_is_reported_for_event_with_prefix([ValueSource(nameof(BadPrefixes))] string prefix) => An_issue_is_reported_for(@"
+using System;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public event EventHandler " + prefix + @"Event { get; set; }
+    }
+}");
+
+        [Test]
+        public void An_issue_is_reported_for_parameter_with_prefix([ValueSource(nameof(BadPrefixes))] string prefix) => An_issue_is_reported_for(@"
+using System;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public void DoSomething(int " + prefix + @"Parameter) { }
+    }
+}");
+
+        [Test]
+        public void An_issue_is_reported_for_method_with_prefix([ValueSource(nameof(BadPrefixes))] string prefix) => An_issue_is_reported_for(@"
+using System;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public void " + prefix + @"Method() { }
+    }
+}");
+
+        [Test]
+        public void An_issue_is_reported_for_class_with_prefix([ValueSource(nameof(BadPrefixes))] string prefix) => An_issue_is_reported_for(@"
+using System;
+
+namespace Bla
+{
+    public class " + prefix + @"Class
+    { }
+}");
+
+        [Test]
+        public void An_issue_is_reported_for_namespace_with_prefix([ValueSource(nameof(BadPrefixes))] string prefix) => An_issue_is_reported_for(@"
+using System;
+
+namespace " + prefix + @"Namespace
+{
+    public class TestMe
+    { }
+}");
+
+        protected override string GetDiagnosticId() => MiKo_1063_AbbreviationsInNameAnalyzer.Id;
+
+        protected override DiagnosticAnalyzer GetObjectUnderTest() => new MiKo_1063_AbbreviationsInNameAnalyzer();
+    }
+}
---
+++ MiKo.Analyzer/Rules/Naming/MiKo_1063_AbbreviationsInNameAnalyzer.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace MiKoSolutions.Analyzers.Rules.Naming
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class MiKo_1063_AbbreviationsInNameAnalyzer : NamingAnalyzer
+    {
+        public const string Id = "MiKo_1063";
+
+        private static readonly Dictionary<string, string> Prefixes = new Dictionary<string, string>
+                                                                          {
+                                                                              { "txt", "text" },
+                                                                              { "Txt", "Text" },
+                                                                          };
+
+        public MiKo_1063_AbbreviationsInNameAnalyzer() : base(Id, (SymbolKind)(-1))
+        {
+        }
+
+        protected override void InitializeCore(AnalysisContext context)
+        {
+            InitializeCore(context, SymbolKind.Namespace, SymbolKind.NamedType, SymbolKind.Method, SymbolKind.Property, SymbolKind.Event, SymbolKind.Field, SymbolKind.Parameter);
+
+            context.RegisterSyntaxNodeAction(AnalyzeLocalDeclarationStatement, SyntaxKind.LocalDeclarationStatement);
+            context.RegisterSyntaxNodeAction(AnalyzeDeclarationPattern, SyntaxKind.DeclarationPattern);
+        }
+
+        protected override IEnumerable<Diagnostic> AnalyzeIdentifiers(SemanticModel semanticModel, params SyntaxToken[] identifiers) => identifiers.Select(_ => _.GetSymbol(semanticModel)).SelectMany(AnalyzeName);
+
+        protected override IEnumerable<Diagnostic> AnalyzeName(INamespaceSymbol symbol) => AnalyzeName(symbol);
+
+        protected override IEnumerable<Diagnostic> AnalyzeName(INamedTypeSymbol symbol) => AnalyzeName(symbol);
+
+        protected override IEnumerable<Diagnostic> AnalyzeName(IMethodSymbol symbol) => AnalyzeName(symbol);
+
+        protected override IEnumerable<Diagnostic> AnalyzeName(IPropertySymbol symbol) => AnalyzeName(symbol);
+
+        protected override IEnumerable<Diagnostic> AnalyzeName(IEventSymbol symbol) => AnalyzeName(symbol);
+
+        protected override IEnumerable<Diagnostic> AnalyzeName(IFieldSymbol symbol) => AnalyzeName(symbol);
+
+        protected override IEnumerable<Diagnostic> AnalyzeName(IParameterSymbol symbol) => AnalyzeName(symbol);
+
+        private IEnumerable<Diagnostic> AnalyzeName(ISymbol symbol)
+        {
+            var symbolName = symbol.Name;
+
+            foreach (var prefix in Prefixes)
+            {
+                if (symbolName.StartsWith(prefix.Key, StringComparison.Ordinal))
+                {
+                    yield return ReportIssue(symbol, prefix.Key, prefix.Value);
+                }
+            }
+        }
+    }
+}
