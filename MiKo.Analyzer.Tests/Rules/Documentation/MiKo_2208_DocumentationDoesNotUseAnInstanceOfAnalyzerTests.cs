41a8f61c2d21439323136c491c307073af10d646
Ralf Koban <rkn@mm-software.com>
2019-04-24 11:33:28 +0000

    Fixes #273

---
+++ MiKo.Analyzer.Tests/Rules/Documentation/MiKo_2208_DocumentationDoesNotUseAnInstanceOfAnalyzerTests.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+
+using NUnit.Framework;
+
+using TestHelper;
+
+namespace MiKoSolutions.Analyzers.Rules.Documentation
+{
+    [TestFixture]
+    public sealed class MiKo_2208_DocumentationDoesNotUseAnInstanceOfAnalyzerTests : CodeFixVerifier
+    {
+        private static readonly string[] Phrases =
+            {
+                "An instance of ",
+                "A instance of ",
+                "The instance of ",
+            };
+
+        private static readonly string[] XmlTags = { "summary", "remarks", "returns", "example", "value", "exception" };
+
+        [Test]
+        public void No_issue_is_reported_for_undocumented_items() => No_issue_is_reported_for(@"
+using System;
+
+public class TestMe
+{
+    public event EventHandler<T> MyEvent;
+
+    public void DoSomething() { }
+
+    public int Age { get; set; }
+
+    private bool m_field;
+}
+");
+
+        [Test]
+        public void No_issue_is_reported_for_correctly_documented_items() => No_issue_is_reported_for(@"
+using System;
+
+/// <summary>Does something.</summary>
+/// <remarks>Does something.</remarks>
+public class TestMe
+{
+    /// <summary>Does something.</summary>
+    /// <remarks>Does something.</remarks>
+    public event EventHandler<T> MyEvent;
+
+    /// <summary>Does something.</summary>
+    /// <remarks>Does something.</remarks>
+    public void DoSomething() { }
+
+    /// <summary>Does something.</summary>
+    /// <remarks>Does something.</remarks>
+    public int Age { get; set; }
+
+    /// <summary>Does something.</summary>
+    /// <remarks>Does something.</remarks>
+    private bool m_field;
+}
+");
+
+        [Test]
+        public void An_issue_is_reported_for_correctly_documented_summary_on_class([ValueSource(nameof(XmlTags))] string tag, [ValueSource(nameof(Phrases))] string phrase) => An_issue_is_reported_for(@"
+using System;
+
+/// <" + tag + @">" + phrase + @".</" + tag + @">
+public class TestMe
+{
+}
+");
+
+        [Test]
+        public void An_issue_is_reported_for_correctly_documented_summary_on_method([ValueSource(nameof(XmlTags))] string tag, [ValueSource(nameof(Phrases))] string phrase) => An_issue_is_reported_for(@"
+using System;
+
+public class TestMe
+{
+    /// <" + tag + @">" + phrase + @".</" + tag + @">
+    public void DoSomething() { }
+}
+");
+
+        [Test]
+        public void An_issue_is_reported_for_correctly_documented_summary_on_property([ValueSource(nameof(XmlTags))] string tag, [ValueSource(nameof(Phrases))] string phrase) => An_issue_is_reported_for(@"
+using System;
+
+public class TestMe
+{
+    /// <" + tag + @">" + phrase + @".</" + tag + @">
+    public int Age { get; set; }
+}
+");
+
+        [Test]
+        public void An_issue_is_reported_for_correctly_documented_summary_on_event([ValueSource(nameof(XmlTags))] string tag, [ValueSource(nameof(Phrases))] string phrase) => An_issue_is_reported_for(@"
+using System;
+
+public class TestMe
+{
+    /// <" + tag + @">" + phrase + @".</" + tag + @">
+    public event EventHandler<T> MyEvent;
+}
+");
+
+        [Test]
+        public void An_issue_is_reported_for_correctly_documented_summary_on_field([ValueSource(nameof(XmlTags))] string tag, [ValueSource(nameof(Phrases))] string phrase) => An_issue_is_reported_for(@"
+using System;
+
+public class TestMe
+{
+    /// <" + tag + @">" + phrase + @".</" + tag + @">
+    private bool m_field;
+}
+");
+
+        protected override string GetDiagnosticId() => MiKo_2208_DocumentationDoesNotUseAnInstanceOfAnalyzer.Id;
+
+        protected override DiagnosticAnalyzer GetObjectUnderTest() => new MiKo_2208_DocumentationDoesNotUseAnInstanceOfAnalyzer();
+    }
+}
