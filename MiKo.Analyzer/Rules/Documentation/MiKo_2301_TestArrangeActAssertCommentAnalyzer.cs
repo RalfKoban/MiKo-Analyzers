529ecfe5fa8af455ae68cab996a894131545f548
Ralf Koban <rkn@mm-software.com>
2019-04-24 15:05:11 +0000

    class stub added

---
+++ MiKo.Analyzer/Rules/Documentation/MiKo_2301_TestArrangeActAssertCommentAnalyzer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace MiKoSolutions.Analyzers.Rules.Documentation
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class MiKo_2301_TestArrangeActAssertCommentAnalyzer : DocumentationAnalyzer
+    {
+        public const string Id = "MiKo_2301";
+
+        public MiKo_2301_TestArrangeActAssertCommentAnalyzer() : base(Id, SymbolKind.Method)
+        {
+        }
+
+        protected override bool ShallAnalyzeMethod(IMethodSymbol symbol) => symbol.IsTestMethod();
+
+        protected override IEnumerable<Diagnostic> AnalyzeMethod(IMethodSymbol symbol, string commentXml) => Enumerable.Empty<Diagnostic>();
+    }
+}
