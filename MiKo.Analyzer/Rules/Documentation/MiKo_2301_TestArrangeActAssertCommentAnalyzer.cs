5f87be7bf154a5fdee4d11d8a615002740e2f106
Ralf Koban <rkn@mm-software.com>
2019-04-25 16:02:07 +0000

    Another base class for MiKo_2301 created to be easily able to inspect for other single line comments as well

--- MiKo.Analyzer/Rules/Documentation/MiKo_2301_TestArrangeActAssertCommentAnalyzer.cs
+++ MiKo.Analyzer/Rules/Documentation/MiKo_2301_TestArrangeActAssertCommentAnalyzer.cs
@@ -1,17 +1,12 @@
-﻿
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System;
 
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace MiKoSolutions.Analyzers.Rules.Documentation
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public sealed class MiKo_2301_TestArrangeActAssertCommentAnalyzer : DocumentationAnalyzer
+    public sealed class MiKo_2301_TestArrangeActAssertCommentAnalyzer : SingleLineCommentAnalyzer
     {
         public const string Id = "MiKo_2301";
 
@@ -30,40 +25,12 @@ namespace MiKoSolutions.Analyzers.Rules.Documentation
                 "verification",
             };
 
-        public MiKo_2301_TestArrangeActAssertCommentAnalyzer() : base(Id, (SymbolKind)(-1))
+        public MiKo_2301_TestArrangeActAssertCommentAnalyzer() : base(Id)
         {
         }
-
-        protected override void InitializeCore(AnalysisContext context) => context.RegisterSyntaxNodeAction(AnalyzeMethod, SyntaxKind.MethodDeclaration);
 
         protected override bool ShallAnalyzeMethod(IMethodSymbol symbol) => symbol.IsTestMethod();
 
-        private void AnalyzeMethod(SyntaxNodeAnalysisContext context)
-        {
-            var node = (MethodDeclarationSyntax)context.Node;
-
-            if (ShallAnalyzeMethod(node.GetEnclosingMethod(context.SemanticModel)))
-            {
-                var issues = AnalyzeSingleLineCommentTrivias(node);
-                foreach (var issue in issues)
-                {
-                    context.ReportDiagnostic(issue);
-                }
-            }
-        }
-
-        private IEnumerable<Diagnostic> AnalyzeSingleLineCommentTrivias(MethodDeclarationSyntax node) => node.DescendantTrivia()
-                                                                                                             .Where(_ => _.IsKind(SyntaxKind.SingleLineCommentTrivia))
-                                                                                                             .Select(_ => AnalyzeSingleLineComment(_, node.Identifier.Text))
-                                                                                                             .Where(_ => _ != null);
-
-        private Diagnostic AnalyzeSingleLineComment(SyntaxTrivia trivia, string methodName)
-        {
-            var comment = trivia.ToFullString().Remove("//").TrimStart();
-
-            return comment.StartsWithAny(Phrases, StringComparison.OrdinalIgnoreCase)
-                       ? ReportIssue(methodName, trivia.GetLocation())
-                       : null;
-        }
+        protected override bool CommentHasIssue(string comment) => comment.StartsWithAny(Phrases, StringComparison.OrdinalIgnoreCase);
     }
 }
---
+++ MiKo.Analyzer/Rules/Documentation/SingleLineCommentAnalyzer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace MiKoSolutions.Analyzers.Rules.Documentation
+{
+    public abstract class SingleLineCommentAnalyzer : DocumentationAnalyzer
+    {
+        protected SingleLineCommentAnalyzer(string diagnosticId) : base(diagnosticId, (SymbolKind)(-1))
+        {
+        }
+
+        protected sealed override void InitializeCore(AnalysisContext context) => context.RegisterSyntaxNodeAction(AnalyzeMethod, SyntaxKind.MethodDeclaration);
+
+        protected abstract bool CommentHasIssue(string comment);
+
+        private void AnalyzeMethod(SyntaxNodeAnalysisContext context)
+        {
+            var node = (MethodDeclarationSyntax)context.Node;
+
+            if (ShallAnalyzeMethod(node.GetEnclosingMethod(context.SemanticModel)))
+            {
+                var issues = AnalyzeSingleLineCommentTrivias(node);
+                foreach (var issue in issues)
+                {
+                    context.ReportDiagnostic(issue);
+                }
+            }
+        }
+
+        private IEnumerable<Diagnostic> AnalyzeSingleLineCommentTrivias(MethodDeclarationSyntax node) => node.DescendantTrivia()
+                                                                                                             .Where(_ => _.IsKind(SyntaxKind.SingleLineCommentTrivia))
+                                                                                                             .Select(_ => AnalyzeSingleLineComment(_, node.Identifier.Text))
+                                                                                                             .Where(_ => _ != null);
+
+        private Diagnostic AnalyzeSingleLineComment(SyntaxTrivia trivia, string methodName)
+        {
+            var comment = trivia.ToFullString().Remove("//").TrimStart();
+
+            return CommentHasIssue(comment)
+                       ? ReportIssue(methodName, trivia.GetLocation())
+                       : null;
+        }
+    }
+}
