65eea32dc897cf3b63696d7e2b683061dcf78650
Ralf Koban <rkn@mm-software.com>
2019-07-24 05:04:33 +0000

    C# 7.1 feature changed ack to C#7 feature (default literal)

--- MiKo.Analyzer/Rules/Maintainability/MiKo_3106_TestAssertsDoNotUseOperatorsAnalyzer.cs
+++ MiKo.Analyzer/Rules/Maintainability/MiKo_3106_TestAssertsDoNotUseOperatorsAnalyzer.cs
@@ -81,7 +81,7 @@ namespace MiKoSolutions.Analyzers.Rules.Maintainability
                 }
             }
 
-            token = default;
+            token = default(SyntaxToken);
             return false;
         }
 
