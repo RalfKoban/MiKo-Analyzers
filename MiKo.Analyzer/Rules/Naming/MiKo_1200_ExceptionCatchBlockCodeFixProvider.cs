d0b125fbaecea6a61a8083b5607f4649fd017f65
Koban, Ralf <rkn@mm-software.com>
2020-07-31 07:46:48 +0000

    MiKo_1200 codefix provider slightly refactored

--- MiKo.Analyzer/Rules/Naming/MiKo_1200_ExceptionCatchBlockCodeFixProvider.cs
+++ MiKo.Analyzer/Rules/Naming/MiKo_1200_ExceptionCatchBlockCodeFixProvider.cs
@@ -17,11 +17,9 @@ namespace MiKoSolutions.Analyzers.Rules.Naming
     public class MiKo_1200_ExceptionCatchBlockCodeFixProvider : CodeFixProvider
     {
         private const string Title = "Rename exception";
-        private const string NewName = "ex";
 
         public sealed override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(MiKo_1200_ExceptionCatchBlockAnalyzer.Id);
 
-        // See https://github.com/dotnet/roslyn/blob/master/docs/analyzers/FixAllProvider.md for more information on Fix All Providers
         public sealed override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
 
         public sealed override async Task RegisterCodeFixesAsync(CodeFixContext context)
@@ -31,24 +29,21 @@ namespace MiKoSolutions.Analyzers.Rules.Naming
             var diagnostic = context.Diagnostics.First();
             var diagnosticSpan = diagnostic.Location.SourceSpan;
 
-            // Find the catch declaration identified by the diagnostic.
-            var declaration = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<CatchDeclarationSyntax>().First();
+            var syntax = root.FindToken(diagnosticSpan.Start).Parent.AncestorsAndSelf().OfType<CatchDeclarationSyntax>().First();
 
-            context.RegisterCodeFix(CodeAction.Create(Title, _ => RenameAsync(context.Document, declaration, _), Title), diagnostic);
+            context.RegisterCodeFix(CodeAction.Create(Title, _ => RenameAsync(context.Document, syntax, _), Title), diagnostic);
         }
 
-        private async Task<Solution> RenameAsync(Document document, CatchDeclarationSyntax declaration, CancellationToken cancellationToken)
+        private async Task<Solution> RenameAsync(Document document, CatchDeclarationSyntax syntax, CancellationToken cancellationToken)
         {
-            // Get the symbol representing the type to be renamed.
             var semanticModel = await document.GetSemanticModelAsync(cancellationToken);
-
-            var symbol = semanticModel.GetDeclaredSymbol(declaration, cancellationToken);
+            var symbol = semanticModel.GetDeclaredSymbol(syntax, cancellationToken);
 
             // Produce a new solution that has all references to that exception renamed, including the declaration.
             var originalSolution = document.Project.Solution;
 
             // Return the new solution with the renamed code
-            return await Renamer.RenameSymbolAsync(originalSolution, symbol, NewName, originalSolution.Workspace.Options, cancellationToken).ConfigureAwait(false);
+            return await Renamer.RenameSymbolAsync(originalSolution, symbol, "ex", originalSolution.Workspace.Options, cancellationToken).ConfigureAwait(false);
         }
     }
 }
