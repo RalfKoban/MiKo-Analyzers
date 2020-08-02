cd907876239c701dcf54f3f8e4e5ff28f78a48c6
Koban, Ralf <rkn@mm-software.com>
2020-08-02 13:53:19 +0000

    MiKo_1004 now has a codefix provider (prototype)

---
+++ MiKo.Analyzer/Rules/Naming/MiKo_1004_CodeFixProvider.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeActions;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MiKoSolutions.Analyzers.Rules.Naming
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(MiKo_1004_CodeFixProvider)), Shared]
+    public sealed class MiKo_1004_CodeFixProvider : NamingCodeFixProvider
+    {
+        public override string FixableDiagnosticId => MiKo_1004_EventNameSuffixAnalyzer.Id;
+
+        protected override CodeAction CreateCodeAction(Document document, IEnumerable<SyntaxNode> syntaxNodes)
+        {
+            var syntax = FindEventSyntax(syntaxNodes.ToList());
+
+            const string Title = "Remove '" + MiKo_1004_EventNameSuffixAnalyzer.Suffix + "' suffix";
+
+            return CodeAction.Create(
+                                     Title,
+                                     _ => RenameSymbolAsync(
+                                                            document,
+                                                            (semanticModel, token) =>
+                                                                {
+                                                                    var symbol = semanticModel.GetDeclaredSymbol(syntax, token);
+                                                                    var newName = MiKo_1004_EventNameSuffixAnalyzer.FindBetterName(symbol);
+
+                                                                    return new Tuple<ISymbol, string>(symbol, newName);
+                                                                },
+                                                            _),
+                                     Title);
+        }
+
+        private static SyntaxNode FindEventSyntax(IEnumerable<SyntaxNode> syntaxNodes)
+        {
+            SyntaxNode eventDeclarationSyntax = syntaxNodes.OfType<EventDeclarationSyntax>().FirstOrDefault();
+            SyntaxNode variableDeclaratorSyntax = syntaxNodes.OfType<VariableDeclaratorSyntax>().FirstOrDefault();
+
+            return eventDeclarationSyntax ?? variableDeclaratorSyntax;
+        }
+    }
+}
