c32018af5fb3dfce23f78959770ef69b58a4fb7d
Koban, Ralf <rkn@mm-software.com>
2020-08-02 10:27:55 +0000

    assertion added so that it is easier to verify failing test

--- MiKo.Analyzer.Tests/Verifiers/CodeFixVerifier.cs
+++ MiKo.Analyzer.Tests/Verifiers/CodeFixVerifier.cs
@@ -51,6 +51,9 @@ namespace TestHelper
         /// <param name="allowNewCompilerDiagnostics">A bool controlling whether or not the test will fail if the CodeFix introduces other warnings after being applied.</param>
         private void VerifyFix(string language, DiagnosticAnalyzer analyzer, CodeFixProvider codeFixProvider, string oldSource, string newSource, int? codeFixIndex, bool allowNewCompilerDiagnostics)
         {
+            Assert.That(analyzer, Is.Not.Null, "Missing Analyzer");
+            Assert.That(codeFixProvider, Is.Not.Null, "Missing CodeFixProvider");
+
             var document = CreateDocument(oldSource, language);
             var analyzerDiagnostics = GetSortedDiagnosticsFromDocuments(analyzer, new[] { document });
             var compilerDiagnostics = GetCompilerDiagnostics(document);
