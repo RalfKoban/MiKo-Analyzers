17cefb3bc119422e706b2af5cbc20c7e1a78d871
Koban, Ralf <rkn@mm-software.com>
2020-08-14 17:00:01 +0000

    Methods moved from ParameterDocumentationCodeFixProvider to base class DocumentationCodeFixProvider

--- MiKo.Analyzer/Rules/Documentation/DocumentationCodeFixProvider.cs
+++ MiKo.Analyzer/Rules/Documentation/DocumentationCodeFixProvider.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace MiKoSolutions.Analyzers.Rules.Documentation
@@ -14,6 +15,26 @@ namespace MiKoSolutions.Analyzers.Rules.Documentation
             return syntaxNodes.SelectMany(_ => _.DescendantNodes(__ => true, true))
                               .OfType<XmlElementSyntax>()
                               .Where(_ => _.StartTag.Name.LocalName.ValueText == startTag);
+        }
+
+        protected static XmlElementSyntax Comment(XmlElementSyntax comment, string[] text) => comment.WithContent(new SyntaxList<XmlNodeSyntax>(SyntaxFactory.XmlText(text[0])));
+
+        protected static XmlElementSyntax CommentWithSeeCRef(XmlElementSyntax comment, string commentStart, TypeSyntax type, string commentEnd)
+        {
+            var content = SyntaxFactory.List<XmlNodeSyntax>()
+                                       .Add(SyntaxFactory.XmlText(commentStart))
+                                       .Add(Cref(Constants.XmlTag.See, type))
+                                       .Add(SyntaxFactory.XmlText(commentEnd));
+
+            return comment.WithContent(content);
+        }
+
+        private static XmlEmptyElementSyntax Cref(string name, TypeSyntax type)
+        {
+            var attribute = SyntaxFactory.XmlCrefAttribute(SyntaxFactory.TypeCref(type.WithoutTrailingTrivia()));
+
+            return SyntaxFactory.XmlEmptyElement(name)
+                                .WithAttributes(new SyntaxList<XmlAttributeSyntax>(attribute));
         }
     }
 }
--- MiKo.Analyzer/Rules/Documentation/ParameterDocumentationCodeFixProvider.cs
+++ MiKo.Analyzer/Rules/Documentation/ParameterDocumentationCodeFixProvider.cs
@@ -2,25 +2,12 @@
 using System.Linq;
 
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace MiKoSolutions.Analyzers.Rules.Documentation
 {
     public abstract class ParameterDocumentationCodeFixProvider : DocumentationCodeFixProvider
     {
-        protected static XmlElementSyntax Comment(XmlElementSyntax comment, string[] text) => comment.WithContent(new SyntaxList<XmlNodeSyntax>(SyntaxFactory.XmlText(text[0])));
-
-        protected static XmlElementSyntax CommentWithSeeCRef(XmlElementSyntax comment, string commentStart, TypeSyntax type, string commentEnd)
-        {
-            var content = SyntaxFactory.List<XmlNodeSyntax>()
-                                       .Add(SyntaxFactory.XmlText(commentStart))
-                                       .Add(Cref(Constants.XmlTag.See, type))
-                                       .Add(SyntaxFactory.XmlText(commentEnd));
-
-            return comment.WithContent(content);
-        }
-
         protected sealed override SyntaxNode GetSyntax(IReadOnlyCollection<SyntaxNode> syntaxNodes)
         {
             var parameterName = syntaxNodes.OfType<ParameterSyntax>().First().Identifier.Text;
@@ -52,13 +39,5 @@ namespace MiKoSolutions.Analyzers.Rules.Documentation
         protected abstract XmlElementSyntax Comment(XmlElementSyntax comment, ParameterSyntax parameter, int index);
 
         private static string GetParameterName(XmlElementSyntax syntax) => syntax.StartTag.Attributes.OfType<XmlNameAttributeSyntax>().First().Identifier.GetName();
-
-        private static XmlEmptyElementSyntax Cref(string name, TypeSyntax type)
-        {
-            var attribute = SyntaxFactory.XmlCrefAttribute(SyntaxFactory.TypeCref(type.WithoutTrailingTrivia()));
-
-            return SyntaxFactory.XmlEmptyElement(name)
-                                .WithAttributes(new SyntaxList<XmlAttributeSyntax>(attribute));
-        }
     }
 }
