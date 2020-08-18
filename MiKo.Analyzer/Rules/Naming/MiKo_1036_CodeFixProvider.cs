f5ffe3dd3c4299bb2c9b1343530c6fdf02fe903e
Koban, Ralf <rkn@mm-software.com>
2020-08-18 10:17:15 +0000

    MiKo_1036 now has a codefix provider

---
+++ MiKo.Analyzer/Rules/Naming/MiKo_1036_CodeFixProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MiKoSolutions.Analyzers.Rules.Naming
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(MiKo_1036_CodeFixProvider)), Shared]
+    public sealed class MiKo_1036_CodeFixProvider : NamingCodeFixProvider
+    {
+        public override string FixableDiagnosticId => MiKo_1036_EventModelSuffixAnalyzer.Id;
+
+        protected override string Title => "Remove 'Model' indicator";
+
+        protected override string GetNewName(ISymbol symbol) => MiKo_1036_EventModelSuffixAnalyzer.FindBetterName((IEventSymbol)symbol);
+
+        protected override SyntaxNode GetSyntax(IReadOnlyCollection<SyntaxNode> syntaxNodes) => syntaxNodes.OfType<VariableDeclaratorSyntax>().FirstOrDefault(); // events are variable declarators
+    }
+}
--- MiKo.Analyzer/Rules/Naming/MiKo_1036_EventModelSuffixAnalyzer.cs
+++ MiKo.Analyzer/Rules/Naming/MiKo_1036_EventModelSuffixAnalyzer.cs
@@ -14,6 +14,8 @@ namespace MiKoSolutions.Analyzers.Rules.Naming
         {
         }
 
+        internal static string FindBetterName(IEventSymbol symbol) => FindBetterNameForEntityMarker(symbol);
+
         protected override bool ShallAnalyze(IEventSymbol symbol) => symbol.IsInterfaceImplementation() is false;
 
         protected override IEnumerable<Diagnostic> AnalyzeName(IEventSymbol symbol) => AnalyzeEntityMarkers(symbol);
