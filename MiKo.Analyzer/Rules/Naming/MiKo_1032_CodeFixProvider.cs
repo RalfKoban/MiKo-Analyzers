8ad3495dbef9a485efb54e2a8b72a76bcc92fcf7
Koban, Ralf <rkn@mm-software.com>
2020-08-18 10:27:36 +0000

    MiKo_1032 now has a codefix provider

---
+++ MiKo.Analyzer/Rules/Naming/MiKo_1032_CodeFixProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MiKoSolutions.Analyzers.Rules.Naming
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(MiKo_1032_CodeFixProvider)), Shared]
+    public sealed class MiKo_1032_CodeFixProvider : NamingCodeFixProvider
+    {
+        public override string FixableDiagnosticId => MiKo_1032_MethodModelSuffixAnalyzer.Id;
+
+        protected override string Title => "Remove 'Model' indicator";
+
+        protected override string GetNewName(ISymbol symbol) => MiKo_1032_MethodModelSuffixAnalyzer.FindBetterName((IMethodSymbol)symbol);
+
+        protected override SyntaxNode GetSyntax(IReadOnlyCollection<SyntaxNode> syntaxNodes) => syntaxNodes.OfType<MethodDeclarationSyntax>().FirstOrDefault();
+    }
+}
