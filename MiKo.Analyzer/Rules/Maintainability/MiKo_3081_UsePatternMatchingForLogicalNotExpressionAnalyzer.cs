469e0d788156e8c321069a36399f1031ab4e1a39
Koban, Ralf <rkn@mm-software.com>
2021-03-29 11:11:55 +0000

    unused using removed

--- MiKo.Analyzer/Rules/Maintainability/MiKo_3081_UsePatternMatchingForLogicalNotExpressionAnalyzer.cs
+++ MiKo.Analyzer/Rules/Maintainability/MiKo_3081_UsePatternMatchingForLogicalNotExpressionAnalyzer.cs
@@ -1,6 +1,4 @@
-﻿using System.Linq.Expressions;
-
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
