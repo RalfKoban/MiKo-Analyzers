e69d8222a9ae8a69722ee4f478ebb89883afddb0
Koban, Ralf <rkn@mm-software.com>
2021-06-03 12:40:11 +0000

    Analyzer and code fix provider for local variables (resolves #364)

---
+++ MiKo.Analyzer.Tests/Rules/Maintainability/MiKo_3203_LocalVariableDeclarationPrecededByBlankLinesAnalyzerTests.cs
@@ -0,0 +1,219 @@
+﻿using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+using NUnit.Framework;
+
+using TestHelper;
+
+namespace MiKoSolutions.Analyzers.Rules.Maintainability
+{
+    [TestFixture]
+    public sealed class MiKo_3203_LocalVariableDeclarationPrecededByBlankLinesAnalyzerTests : CodeFixVerifier
+    {
+        [Test]
+        public void No_issue_is_reported_for_method_call() => No_issue_is_reported_for(@"
+using NUnit.Framework;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public void DoSomething()
+        {
+            DoSomething();
+        }
+    }
+}
+");
+
+        [Test]
+        public void No_issue_is_reported_for_variable_as_first_statement() => No_issue_is_reported_for(@"
+using NUnit.Framework;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public void DoSomething(bool something)
+        {
+            var x = 1;
+
+            DoSomething();
+        }
+    }
+}
+");
+
+        [Test]
+        public void An_issue_is_reported_for_variable_preceded_by_if_block() => An_issue_is_reported_for(@"
+using NUnit.Framework;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public void DoSomething(bool something)
+        {
+            if (something)
+            {
+                // some comment
+            }
+            var x = 1;
+        }
+    }
+}
+");
+
+        [Test]
+        public void An_issue_is_reported_for_variable_preceded_by_method_call() => An_issue_is_reported_for(@"
+using NUnit.Framework;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public void DoSomething(bool something)
+        {
+            DoSomething();
+            var x = 1;
+        }
+    }
+}
+");
+
+        [Test]
+        public void Code_gets_fixed_for_missing_preceding_line_after_if_block()
+        {
+            const string OriginalCode = @"
+using NUnit.Framework;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public void DoSomething(bool something)
+        {
+            if (something)
+            {
+                // some comment
+            }
+            var x = 1;
+        }
+    }
+}
+";
+
+            const string FixedCode = @"
+using NUnit.Framework;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public void DoSomething(bool something)
+        {
+            if (something)
+            {
+                // some comment
+            }
+
+            var x = 1;
+        }
+    }
+}
+";
+
+            VerifyCSharpFix(OriginalCode, FixedCode);
+        }
+
+        [Test]
+        public void Code_gets_fixed_for_missing_preceding_line_after_method_call()
+        {
+            const string OriginalCode = @"
+using NUnit.Framework;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public void DoSomething(bool something)
+        {
+            DoSomething(something);
+            var x = 1;
+            DoSomething(something);
+        }
+    }
+}
+";
+
+            const string FixedCode = @"
+using NUnit.Framework;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public void DoSomething(bool something)
+        {
+            DoSomething(something);
+
+            var x = 1;
+            DoSomething(something);
+        }
+    }
+}
+";
+
+            VerifyCSharpFix(OriginalCode, FixedCode);
+        }
+
+        [Test]
+        public void Code_gets_fixed_for_multiple_variables_with_missing_preceding_line()
+        {
+            const string OriginalCode = @"
+using NUnit.Framework;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public void DoSomething(bool something)
+        {
+            DoSomething(something);
+            var x = something;
+            var y = something;
+            DoSomething(something);
+        }
+    }
+}
+";
+
+            const string FixedCode = @"
+using NUnit.Framework;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public void DoSomething(bool something)
+        {
+            DoSomething(something);
+
+            var x = something;
+            var y = something;
+            DoSomething(something);
+        }
+    }
+}
+";
+
+            VerifyCSharpFix(OriginalCode, FixedCode);
+        }
+
+        protected override string GetDiagnosticId() => MiKo_3203_LocalVariableDeclarationPrecededByBlankLinesAnalyzer.Id;
+
+        protected override DiagnosticAnalyzer GetObjectUnderTest() => new MiKo_3203_LocalVariableDeclarationPrecededByBlankLinesAnalyzer();
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider() => new MiKo_3203_CodeFixProvider();
+    }
+}
--- MiKo.Analyzer/Resources.Designer.cs
+++ MiKo.Analyzer/Resources.Designer.cs
@@ -8989,6 +8989,42 @@ namespace MiKoSolutions.Analyzers {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Precede with blank line.
+        /// </summary>
+        public static string MiKo_3203_CodeFixTitle {
+            get {
+                return ResourceManager.GetString("MiKo_3203_CodeFixTitle", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to To separate local variables visually from executing code, they should be preceded by blank lines. That makes them easier to spot..
+        /// </summary>
+        public static string MiKo_3203_Description {
+            get {
+                return ResourceManager.GetString("MiKo_3203_Description", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Precede local variable with a blank line.
+        /// </summary>
+        public static string MiKo_3203_MessageFormat {
+            get {
+                return ResourceManager.GetString("MiKo_3203_MessageFormat", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Local variable statements should be preceded by blank lines..
+        /// </summary>
+        public static string MiKo_3203_Title {
+            get {
+                return ResourceManager.GetString("MiKo_3203_Title", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Namespaces should have only a limited depth. If a namespace hierarchy becomes too deep, then the namespaces are probably too concrete or special to make sense.
         ///Such namespaces should be flattened..
         /// </summary>
