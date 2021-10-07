cdb067b5fbea9feecb18dddf699f03815b26e5eb
Koban, Ralf <rkn@mm-software.com>
2021-10-07 07:24:06 +0000

    more tests for Pluralizer

--- MiKo.Analyzer.Tests/Rules/Naming/PluralizerTests.cs
+++ MiKo.Analyzer.Tests/Rules/Naming/PluralizerTests.cs
@@ -6,6 +6,7 @@ namespace MiKoSolutions.Analyzers.Rules.Naming
     public sealed class PluralizerTests
     {
         [TestCase("Access", ExpectedResult = "Accesses")]
+        [TestCase("Array", ExpectedResult = "Arrays")]
         [TestCase("Child", ExpectedResult = "Children")]
         [TestCase("Children", ExpectedResult = "Children")]
         [TestCase("Data", ExpectedResult = "Data")]
@@ -14,12 +15,22 @@ namespace MiKoSolutions.Analyzers.Rules.Naming
         [TestCase("Index", ExpectedResult = "Indices")]
         [TestCase("Indices", ExpectedResult = "Indices")]
         [TestCase("Information", ExpectedResult = "Information")]
+        [TestCase("Informations", ExpectedResult = "Information")]
         [TestCase("Key", ExpectedResult = "Keys")]
         [TestCase("Keys", ExpectedResult = "Keys")]
         [TestCase("Mech", ExpectedResult = "Mechs")]
         [TestCase("Nested", ExpectedResult = "Nested")]
         [TestCase("Security", ExpectedResult = "Securities")]
         [TestCase("Securitys", ExpectedResult = "Securities")]
+        [TestCase("complete", ExpectedResult = "all")]
+        [TestCase("bases", ExpectedResult = "items")]
+        [TestCase("_bases", ExpectedResult = "_items")]
+        [TestCase("m_bases", ExpectedResult = "m_items")]
+        [TestCase("sources", ExpectedResult = "source")]
+        [TestCase("_sources", ExpectedResult = "_source")]
+        [TestCase("m_sources", ExpectedResult = "m_source")]
+        [TestCase("namesToConvert", ExpectedResult = "names")]
+        [TestCase("itemsToModel", ExpectedResult = "items")]
         public string Creates_correct_plural_name_(string singularName) => Pluralizer.GetPluralName(singularName);
     }
 }
