7d2fd9250f7379f7f380d3e44ea426d38610b062
Koban, Ralf <rkn@mm-software.com>
2021-11-29 08:31:34 +0000

    MiKo_2302 is now non-concurrent

--- MiKo.Analyzer/Rules/Documentation/MiKo_2302_CommentedOutCodeAnalyzer.cs
+++ MiKo.Analyzer/Rules/Documentation/MiKo_2302_CommentedOutCodeAnalyzer.cs
@@ -12,6 +12,8 @@ namespace MiKoSolutions.Analyzers.Rules.Documentation
         {
         }
 
+        protected override bool CanRunConcurrently => false;
+
         protected override void PrepareAnalyzeMethod(Compilation compilation) => CodeDetector.RefreshKnownTypes(compilation);
 
         protected override bool CommentHasIssue(string comment, SemanticModel semanticModel) => CodeDetector.IsCommentedOutCodeLine(comment);
