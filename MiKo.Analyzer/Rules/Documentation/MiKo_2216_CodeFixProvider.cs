91f626b8f1ed7ba3ff121351a1101a9337d1b27e
Koban, Ralf <rkn@mm-software.com>
2021-12-08 10:00:35 +0000

    new rule for incorrect usage of <param> (resolves #440)

---
+++ MiKo.Analyzer/Rules/Documentation/MiKo_2216_CodeFixProvider.cs
@@ -0,0 +1,47 @@
+﻿using System.Composition;
+using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MiKoSolutions.Analyzers.Rules.Documentation
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(MiKo_2216_CodeFixProvider)), Shared]
+    public sealed class MiKo_2216_CodeFixProvider : OverallDocumentationCodeFixProvider
+    {
+        public override string FixableDiagnosticId => MiKo_2216_ParamInsteadOfParamRefAnalyzer.Id;
+
+        protected override string Title => Resources.MiKo_2216_CodeFixTitle;
+
+        protected override DocumentationCommentTriviaSyntax GetUpdatedSyntax(Document document, DocumentationCommentTriviaSyntax syntax, Diagnostic diagnostic)
+        {
+            var elements = MiKo_2216_ParamInsteadOfParamRefAnalyzer.GetProblematicElements(syntax);
+
+            var issue = elements.First(_ => _.GetLocation().Equals(diagnostic.Location));
+
+            // TODO RKN: use this for bulk replace: return syntax.ReplaceNodes(elements, (original, rewritten) => GetUpdatedSyntax(original));
+            return syntax.ReplaceNode(issue, GetUpdatedSyntax(issue));
+        }
+
+        private static SyntaxNode GetUpdatedSyntax(SyntaxNode syntax)
+        {
+            switch (syntax)
+            {
+                case XmlElementSyntax xes:
+                    return SyntaxFactory.XmlParamRefElement(GetParameterName(xes));
+
+                case XmlEmptyElementSyntax xees:
+                    return SyntaxFactory.XmlParamRefElement(GetParameterName(xees));
+
+                default:
+                    return syntax;
+            }
+        }
+
+        private static string GetParameterName(XmlElementSyntax syntax) => syntax.GetAttributes<XmlNameAttributeSyntax>().First().Identifier.GetName();
+
+        private static string GetParameterName(XmlEmptyElementSyntax syntax) => syntax.Attributes.OfType<XmlNameAttributeSyntax>().First().Identifier.GetName();
+    }
+}
---
+++ MiKo.Analyzer/Rules/Documentation/MiKo_2216_ParamInsteadOfParamRefAnalyzer.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace MiKoSolutions.Analyzers.Rules.Documentation
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class MiKo_2216_ParamInsteadOfParamRefAnalyzer : OverallDocumentationAnalyzer
+    {
+        public const string Id = "MiKo_2216";
+
+        public MiKo_2216_ParamInsteadOfParamRefAnalyzer() : base(Id)
+        {
+        }
+
+        internal static IEnumerable<SyntaxNode> GetProblematicElements(DocumentationCommentTriviaSyntax comment)
+        {
+            foreach (var element in comment.DescendantNodes())
+            {
+                if (element.IsXmlTag(Constants.XmlTag.Param))
+                {
+                    if (element.Parent is XmlElementSyntax)
+                    {
+                        yield return element;
+                    }
+                }
+            }
+        }
+
+        protected override IEnumerable<Diagnostic> AnalyzeComment(ISymbol symbol, string commentXml)
+        {
+            var comment = symbol.GetSyntax().DescendantNodes(_ => true, true).OfType<DocumentationCommentTriviaSyntax>().FirstOrDefault();
+            if (comment is null)
+            {
+                // it might be that there is no documentation comment available (while the comment XML contains something like " <member name='xyz' ...> "
+                yield break;
+            }
+
+            foreach (var element in GetProblematicElements(comment))
+            {
+                yield return Issue(element);
+            }
+        }
+    }
+}
