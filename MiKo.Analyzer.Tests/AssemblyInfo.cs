d416af10b6ea9caab6730c43de062f64bd51a960
Koban, Ralf <rkn@mm-software.com>
2021-12-17 15:31:42 +0000

    NCrunch timeout set to 30s

--- MiKo.Analyzer.Tests/AssemblyInfo.cs
+++ MiKo.Analyzer.Tests/AssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using NUnit.Framework;
 
 #if NCRUNCH
-[assembly: Timeout(20 * 1000)] // default timeout of 20 seconds
+[assembly: Timeout(30 * 1000)] // default timeout of 30 seconds
 #else
 [assembly: Timeout(60 * 1000)] // default timeout of 60 seconds
 #endif
