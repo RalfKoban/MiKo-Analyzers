4fa45b478f91446d915cf65e5a6c2f65773781ab
Koban, Ralf <rkn@mm-software.com>
2022-01-27 15:02:06 +0000

    MiKo_1015 is now aware of local functions (part of #416)

--- MiKo.Analyzer.Tests/Rules/Naming/MiKo_1015_InitMethodsAnalyzerTests.cs
+++ MiKo.Analyzer.Tests/Rules/Naming/MiKo_1015_InitMethodsAnalyzerTests.cs
@@ -20,6 +20,19 @@ public class TestMe
 }
 ");
 
+        [TestCase("DoSomething")]
+        [TestCase("Initialize")]
+        [TestCase("InitializeSomething")]
+        public void No_issue_is_reported_for_correctly_named_local_function_(string methodName) => No_issue_is_reported_for(@"
+public class TestMe
+{
+    public void Something()
+    {
+        void " + methodName + @"() { }
+    }
+}
+");
+
         [TestCase("Init")]
         [TestCase("Init2")]
         [TestCase("InitSomething")]
@@ -30,10 +43,28 @@ public class TestMe
 }
 ");
 
+        [TestCase("Init")]
+        [TestCase("Init2")]
+        [TestCase("InitSomething")]
+        public void An_issue_is_reported_for_wrong_named_local_function_(string methodName) => An_issue_is_reported_for(@"
+public class TestMe
+{
+    public void Something()
+    {
+        void " + methodName + @"() { }
+    }
+}
+");
+
         [TestCase("class TestMe { void Init() { } }", "class TestMe { void Initialize() { } }")]
         [TestCase("class TestMe { void Init2() { } }", "class TestMe { void Initialize2() { } }")]
         [TestCase("class TestMe { void InitSomething() { } }", "class TestMe { void InitializeSomething() { } }")]
-        public void Code_gets_fixed_(string originalCode, string fixedCode) => VerifyCSharpFix(originalCode, fixedCode);
+        public void Code_gets_fixed_for_method_(string originalCode, string fixedCode) => VerifyCSharpFix(originalCode, fixedCode);
+
+        [TestCase("class TestMe { void Something() { void Init() { } } }", "class TestMe { void Something() { void Initialize() { } } }")]
+        [TestCase("class TestMe { void Something() { void Init2() { } } }", "class TestMe { void Something() { void Initialize2() { } } }")]
+        [TestCase("class TestMe { void Something() { void InitSomething() { } } }", "class TestMe { void Something() { void InitializeSomething() { } } }")]
+        public void Code_gets_fixed_for_local_function_(string originalCode, string fixedCode) => VerifyCSharpFix(originalCode, fixedCode);
 
         protected override string GetDiagnosticId() => MiKo_1015_InitMethodsAnalyzer.Id;
 
