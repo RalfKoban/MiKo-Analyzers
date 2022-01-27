9f2c2b72a4e48f119d0f496500732d9db4c4e128
Koban, Ralf <rkn@mm-software.com>
2022-01-27 15:55:39 +0000

    MiKo_1032 is now aware of local functions (part of #416)

--- MiKo.Analyzer.Tests/Rules/Naming/MiKo_1032_MethodModelSuffixAnalyzerTests.cs
+++ MiKo.Analyzer.Tests/Rules/Naming/MiKo_1032_MethodModelSuffixAnalyzerTests.cs
@@ -21,6 +21,20 @@ public class TestMe
 }
 ");
 
+        [TestCase("CreateViewModel")]
+        [TestCase("CreateViewModels")]
+        [TestCase("DoSomething")]
+        [TestCase("EnableModelessStuff")]
+        public void No_issue_is_reported_for_valid_local_function_(string name) => No_issue_is_reported_for(@"
+public class TestMe
+{
+    public void DoSomething()
+    {
+        void " + name + @"() { }
+    }
+}
+");
+
         [TestCase("CreateModel")]
         [TestCase("CreateModels")]
         [TestCase("CreateitemModel")]
@@ -32,9 +46,27 @@ public class TestMe
 }
 ");
 
+        [TestCase("CreateModel")]
+        [TestCase("CreateModels")]
+        [TestCase("CreateitemModel")]
+        [TestCase("CreateModelItem")]
+        public void An_issue_is_reported_for_invalid_local_function_(string name) => An_issue_is_reported_for(@"
+public class TestMe
+{
+    public void DoSomething()
+    {
+        void " + name + @"() { }
+    }
+}
+");
+
         [TestCase("class TestMe { public void CreateModel() { } }", "class TestMe { public void Create() { } }")]
         [TestCase("class TestMe { public void CreateModelItem() { } }", "class TestMe { public void CreateItem() { } }")]
-        public void Code_gets_fixed_(string originalCode, string fixedCode) => VerifyCSharpFix(originalCode, fixedCode);
+        public void Code_gets_fixed_for_method_(string originalCode, string fixedCode) => VerifyCSharpFix(originalCode, fixedCode);
+
+        [TestCase("class TestMe { public void DoSomething() { void CreateModel() { } } }", "class TestMe { public void DoSomething() { void Create() { } } }")]
+        [TestCase("class TestMe { public void DoSomething() { void CreateModelItem() { } } }", "class TestMe { public void DoSomething() { void CreateItem() { } } }")]
+        public void Code_gets_fixed_for_local_function_(string originalCode, string fixedCode) => VerifyCSharpFix(originalCode, fixedCode);
 
         protected override string GetDiagnosticId() => MiKo_1032_MethodModelSuffixAnalyzer.Id;
 
