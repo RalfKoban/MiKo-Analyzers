269af66ca26bd2916b04c4ff59ed4de97594cce6
Koban, Ralf <rkn@mm-software.com>
2022-01-30 11:38:14 +0000

    slight refactoring of naming length analyzers

--- MiKo.Analyzer/Rules/Naming/MiKo_1022_ParameterNameLengthAnalyzer.cs
+++ MiKo.Analyzer/Rules/Naming/MiKo_1022_ParameterNameLengthAnalyzer.cs
@@ -11,7 +11,7 @@ namespace MiKoSolutions.Analyzers.Rules.Naming
     {
         public const string Id = "MiKo_1022";
 
-        public MiKo_1022_ParameterNameLengthAnalyzer() : base(Id, SymbolKind.Method, Constants.MaxNamingLengths.Parameters)
+        public MiKo_1022_ParameterNameLengthAnalyzer() : base(Id, SymbolKind.Method, Constants.MaxNamingLengths.Parameters) // we need to use methods here as - unfortunately - parameters on local functions are not considered
         {
         }
 
