1d00c803716a2fa1131fd3e7ce7642a4baece1e1
Koban, Ralf <rkn@mm-software.com>
2022-07-06 09:40:30 +0200

    MiKo_1111 now has a rudimentary codefix provider (part of #483)

--- MiKo.Analyzer.Shared/Rules/Naming/MiKo_1107_TestMethodsPascalCasingAnalyzer.cs
+++ MiKo.Analyzer.Shared/Rules/Naming/MiKo_1107_TestMethodsPascalCasingAnalyzer.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Diagnostics;
@@ -22,7 +21,7 @@ namespace MiKoSolutions.Analyzers.Rules.Naming
 
         protected override bool IsUnitTestAnalyzer => true;
 
-        internal static string FindBetterName(ISymbol symbol) => FindBetterName(symbol.Name);
+        internal static string FindBetterName(ISymbol symbol) => NamesFinder.FindBetterTestName(symbol.Name);
 
         protected override bool ShallAnalyze(IMethodSymbol symbol) => base.ShallAnalyze(symbol) && symbol.IsTestMethod();
 
@@ -50,113 +49,6 @@ namespace MiKoSolutions.Analyzers.Rules.Naming
 
                 yield return Issue(symbol);
             }
-        }
-
-        private static string FindBetterName(string symbolName)
-        {
-            if (symbolName.Length < 2)
-            {
-                return symbolName;
-            }
-
-            var correctedSymbolName = symbolName
-                                            .Replace("MustBe", "Is")
-                                            .Replace("MustNotBe", "IsNot")
-                                            .Replace("ShallBe", "Is")
-                                            .Replace("ShallNotBe", "IsNot")
-                                            .Replace("ShouldBe", "Is")
-                                            .Replace("ShouldNotBe", "IsNot")
-                                            .Replace("ShouldFail", "Fails")
-                                            .Replace("ShouldReturn", "Returns")
-                                            .Replace("ShouldThrow", "Throws");
-
-            var multipleUpperCases = false;
-
-            const int CharacterToStartWith = 1;
-
-            var characters = new List<char>(correctedSymbolName);
-            for (var index = CharacterToStartWith; index < characters.Count; index++)
-            {
-                var c = characters[index];
-
-                if (c == '_')
-                {
-                    // keep the existing underline
-                    continue;
-                }
-
-                if (c.IsUpperCase())
-                {
-                    if (index == CharacterToStartWith)
-                    {
-                        // multiple upper cases in a line at beginning of the name, so do not flip
-                        multipleUpperCases = true;
-                    }
-
-                    if (multipleUpperCases)
-                    {
-                        // let's see if we start with an IXyz interface
-                        if (characters[index - 1] == 'I')
-                        {
-                            // seems we are in an IXyz interface
-                            multipleUpperCases = false;
-                        }
-
-                        continue;
-                    }
-
-                    // let's consider an upper-case 'A' as a special situation as that is a single word
-                    var isSpecialCharA = c == 'A';
-
-                    multipleUpperCases = isSpecialCharA is false;
-
-                    var nextC = c.ToLowerCase();
-
-                    var nextIndex = index + 1;
-                    if (nextIndex >= characters.Count || (nextIndex < characters.Count && characters[nextIndex].IsUpperCase()) && isSpecialCharA is false)
-                    {
-                        // multiple upper cases in a line, so do not flip
-                        nextC = c;
-                    }
-
-                    if (characters[index - 1] == '_')
-                    {
-                        characters[index] = nextC;
-                    }
-                    else
-                    {
-                        // only add an underline if we not already have one
-                        characters[index] = '_';
-                        index++;
-                        characters.Insert(index, nextC);
-                    }
-                }
-                else
-                {
-                    if (multipleUpperCases && characters[index - 1].IsUpperCase())
-                    {
-                        // we are behind multiple upper cases in a line, so add an underline
-                        characters[index++] = '_';
-                        characters.Insert(index, c);
-                    }
-
-                    multipleUpperCases = false;
-                }
-            }
-
-            // fix some corrections, such as for known exceptions
-            var result = new string(characters.ToArray())
-                         .Replace("argument_null_exception", nameof(ArgumentNullException))
-                         .Replace("argument_exception", nameof(ArgumentException))
-                         .Replace("argument_out_of_range_exception", nameof(ArgumentOutOfRangeException))
-                         .Replace("invalid_operation_exception", nameof(InvalidOperationException))
-                         .Replace("object_disposed_exception", nameof(ObjectDisposedException))
-                         .Replace("not_supported_exception", nameof(NotSupportedException))
-                         .Replace("not_implemented_exception", nameof(NotImplementedException))
-                         .Replace("task_canceled_exception", nameof(TaskCanceledException))
-                         .Replace("operation_canceled_exception", nameof(OperationCanceledException));
-
-            return string.Intern(result);
         }
     }
 }
--- MiKo.Analyzer.Shared/Rules/Naming/NamesFinder.cs
+++ MiKo.Analyzer.Shared/Rules/Naming/NamesFinder.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 
 using Microsoft.CodeAnalysis;
 
@@ -42,6 +43,114 @@ namespace MiKoSolutions.Analyzers.Rules.Naming
             }
 
             return propertyNames;
+        }
+
+        internal static string FindBetterTestName(string symbolName)
+        {
+            if (symbolName.Length < 3)
+            {
+                return symbolName;
+            }
+
+            var correctedSymbolName = symbolName
+                                            .Replace("MustBe", "Is")
+                                            .Replace("MustNotBe", "IsNot")
+                                            .Replace("ShallBe", "Is")
+                                            .Replace("ShallNotBe", "IsNot")
+                                            .Replace("ShouldBe", "Is")
+                                            .Replace("ShouldNotBe", "IsNot")
+                                            .Replace("ShouldFail", "Fails")
+                                            .Replace("ShouldReturn", "Returns")
+                                            .Replace("ShouldThrow", "Throws")
+                                            .Replace("IsExceptional", "ThrowsException");
+
+            var multipleUpperCases = false;
+
+            const int CharacterToStartWith = 1;
+
+            var characters = new List<char>(correctedSymbolName);
+            for (var index = CharacterToStartWith; index < characters.Count; index++)
+            {
+                var c = characters[index];
+
+                if (c == '_')
+                {
+                    // keep the existing underline
+                    continue;
+                }
+
+                if (c.IsUpperCase())
+                {
+                    if (index == CharacterToStartWith)
+                    {
+                        // multiple upper cases in a line at beginning of the name, so do not flip
+                        multipleUpperCases = true;
+                    }
+
+                    if (multipleUpperCases)
+                    {
+                        // let's see if we start with an IXyz interface
+                        if (characters[index - 1] == 'I')
+                        {
+                            // seems we are in an IXyz interface
+                            multipleUpperCases = false;
+                        }
+
+                        continue;
+                    }
+
+                    // let's consider an upper-case 'A' as a special situation as that is a single word
+                    var isSpecialCharA = c == 'A';
+
+                    multipleUpperCases = isSpecialCharA is false;
+
+                    var nextC = c.ToLowerCase();
+
+                    var nextIndex = index + 1;
+                    if (nextIndex >= characters.Count || (nextIndex < characters.Count && characters[nextIndex].IsUpperCase()) && isSpecialCharA is false)
+                    {
+                        // multiple upper cases in a line, so do not flip
+                        nextC = c;
+                    }
+
+                    if (characters[index - 1] == '_')
+                    {
+                        characters[index] = nextC;
+                    }
+                    else
+                    {
+                        // only add an underline if we not already have one
+                        characters[index] = '_';
+                        index++;
+                        characters.Insert(index, nextC);
+                    }
+                }
+                else
+                {
+                    if (multipleUpperCases && characters[index - 1].IsUpperCase())
+                    {
+                        // we are behind multiple upper cases in a line, so add an underline
+                        characters[index++] = '_';
+                        characters.Insert(index, c);
+                    }
+
+                    multipleUpperCases = false;
+                }
+            }
+
+            // fix some corrections, such as for known exceptions
+            var result = new string(characters.ToArray())
+                         .Replace("argument_null_exception", nameof(ArgumentNullException))
+                         .Replace("argument_exception", nameof(ArgumentException))
+                         .Replace("argument_out_of_range_exception", nameof(ArgumentOutOfRangeException))
+                         .Replace("invalid_operation_exception", nameof(InvalidOperationException))
+                         .Replace("object_disposed_exception", nameof(ObjectDisposedException))
+                         .Replace("not_supported_exception", nameof(NotSupportedException))
+                         .Replace("not_implemented_exception", nameof(NotImplementedException))
+                         .Replace("task_canceled_exception", nameof(TaskCanceledException))
+                         .Replace("operation_canceled_exception", nameof(OperationCanceledException));
+
+            return string.Intern(result);
         }
 
         private static string GetRegisteredName(IFieldSymbol symbol, string invocation)
