d887a342e8db49ca6a5c19099039dcdbf5e0ddaa
Koban, Ralf <rkn@mm-software.com>
2023-03-05 12:48:07 +0000

    MiKo_2000 now underlines the problematic XML elements

--- MiKo.Analyzer.Shared/Rules/Documentation/MiKo_2000_MalformedDocumentationAnalyzer.cs
+++ MiKo.Analyzer.Shared/Rules/Documentation/MiKo_2000_MalformedDocumentationAnalyzer.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
@@ -17,8 +17,9 @@ namespace MiKoSolutions.Analyzers.Rules.Documentation
         {
         }
 
-        protected override IEnumerable<Diagnostic> AnalyzeComment(ISymbol symbol, Compilation compilation, string commentXml, DocumentationCommentTriviaSyntax comment) => commentXml.StartsWith("<!--", StringComparison.OrdinalIgnoreCase)
-                                                                                                                                                                               ? new[] { Issue(symbol) }
-                                                                                                                                                                               : Enumerable.Empty<Diagnostic>();
+        protected override IEnumerable<Diagnostic> AnalyzeComment(ISymbol symbol, Compilation compilation, string commentXml, DocumentationCommentTriviaSyntax comment)
+        {
+            return comment.DescendantTokens(SyntaxKind.XmlEntityLiteralToken).Select(_ => Issue(_));
+        }
     }
 }
