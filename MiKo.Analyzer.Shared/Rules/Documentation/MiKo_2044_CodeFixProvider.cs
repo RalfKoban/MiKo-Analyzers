ba7352f7e905947b17ebb60fb32772ae1d70f7d6
Koban, Ralf <rkn@mm-software.com>
2023-03-18 13:39:27 +0000

    refactored MiKo_2044

--- MiKo.Analyzer.Shared/Rules/Documentation/MiKo_2044_CodeFixProvider.cs
+++ MiKo.Analyzer.Shared/Rules/Documentation/MiKo_2044_CodeFixProvider.cs
@@ -5,50 +5,24 @@ using System.Linq;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
 
 namespace MiKoSolutions.Analyzers.Rules.Documentation
 {
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(MiKo_2044_CodeFixProvider)), Shared]
-    public sealed class MiKo_2044_CodeFixProvider : OverallDocumentationCodeFixProvider
+    public sealed class MiKo_2044_CodeFixProvider : DocumentationCodeFixProvider
     {
-        private static readonly HashSet<string> TagNames = new HashSet<string>
-                                                               {
-                                                                   Constants.XmlTag.See,
-                                                                   Constants.XmlTag.SeeAlso,
-                                                               };
-
         public override string FixableDiagnosticId => MiKo_2044_InvalidSeeParameterInXmlAnalyzer.Id;
 
         protected override string Title => Resources.MiKo_2044_CodeFixTitle;
 
-        protected override DocumentationCommentTriviaSyntax GetUpdatedSyntax(CodeFixContext context, DocumentationCommentTriviaSyntax syntax, Diagnostic diagnostic)
+        protected override SyntaxNode GetSyntax(IEnumerable<SyntaxNode> syntaxNodes) => syntaxNodes.First();
+
+        protected override SyntaxNode GetUpdatedSyntax(CodeFixContext context, SyntaxNode syntax, Diagnostic issue)
         {
-            var method = syntax.FirstAncestorOrSelf<MethodDeclarationSyntax>();
+            var cref = syntax.GetCref();
+            var name = cref.GetCrefType().GetName();
 
-            if (method is null)
-            {
-                return syntax;
-            }
-
-            var parameters = method.ParameterList.Parameters.ToHashSet(_ => _.GetName());
-
-            var map = new Dictionary<XmlEmptyElementSyntax, string>();
-
-            var tags = syntax.GetEmptyXmlSyntax(TagNames);
-
-            foreach (var tag in tags)
-            {
-                foreach (var parameterName in tag.Attributes
-                                                 .SelectMany(_ => _.ChildNodes<NameMemberCrefSyntax>())
-                                                 .Select(_ => _.Name.ToString())
-                                                 .Where(parameters.Contains))
-                {
-                    map.Add(tag, parameterName);
-                }
-            }
-
-            return syntax.ReplaceNodes(map.Keys, (_, __) => SyntaxFactory.XmlParamRefElement(map[_]));
+            return SyntaxFactory.XmlParamRefElement(name);
         }
     }
 }
