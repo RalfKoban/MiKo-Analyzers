10ddfa5be9d5b1262ac331a041dbff5a704d4506
Koban, Ralf <rkn@mm-software.com>
2023-04-24 12:06:18 +0000

    MiKo_1000 is now aware of the suffix 'Arg' and 'Base' to do a proposal

--- MiKo.Analyzer.Shared/Rules/Naming/MiKo_1000_EventArgsTypeAnalyzer.cs
+++ MiKo.Analyzer.Shared/Rules/Naming/MiKo_1000_EventArgsTypeAnalyzer.cs
@@ -15,11 +15,22 @@ namespace MiKoSolutions.Analyzers.Rules.Naming
 
         private const string CorrectSuffix = nameof(EventArgs);
 
-        private const string Arg = "Arg";
-        private const string Args = "Args";
-        private const string EventsArgs = "EventsArgs";
-        private const string EventArg = "EventArg";
-        private const string EventsArg = "EventsArg";
+        private const string Arg = nameof(Arg);
+        private const string Args = nameof(Args);
+        private const string Base = nameof(Base);
+        private const string BaseArg = nameof(BaseArg);
+        private const string BaseArgs = nameof(BaseArgs);
+        private const string EventBaseArg = nameof(EventBaseArg);
+        private const string EventsBaseArg = nameof(EventsBaseArg);
+        private const string EventBaseArgs = nameof(EventBaseArgs);
+        private const string EventsBaseArgs = nameof(EventsBaseArgs);
+        private const string EventsArgs = nameof(EventsArgs);
+        private const string EventArg = nameof(EventArg);
+        private const string EventsArg = nameof(EventsArg);
+        private const string EventArgBase = nameof(EventArgBase);
+        private const string EventArgsBase = nameof(EventArgsBase);
+        private const string EventsArgBase = nameof(EventsArgBase);
+        private const string EventsArgsBase = nameof(EventsArgsBase);
 
         public MiKo_1000_EventArgsTypeAnalyzer() : base(Id, SymbolKind.NamedType)
         {
@@ -43,26 +54,31 @@ namespace MiKoSolutions.Analyzers.Rules.Naming
 
         private static string GetNameWithoutSuffix(string name)
         {
-            if (name.EndsWith(Args, StringComparison.Ordinal))
-            {
-                if (name.EndsWith(EventsArgs, StringComparison.Ordinal))
-                {
-                    return name.WithoutSuffix(EventsArgs);
-                }
-
-                return name.WithoutSuffix(Args);
-            }
-
             if (name.EndsWith(Arg, StringComparison.Ordinal))
             {
-                if (name.EndsWith(EventArg, StringComparison.Ordinal))
-                {
-                    return name.WithoutSuffix(EventArg);
-                }
+                return GetNameWithoutSuffixes(name, EventArg, EventsArg, EventBaseArg, EventsBaseArg, BaseArg, Arg);
+            }
 
-                if (name.EndsWith(EventsArg, StringComparison.Ordinal))
+            if (name.EndsWith(Args, StringComparison.Ordinal))
+            {
+                return GetNameWithoutSuffixes(name, EventsArgs, EventBaseArgs, EventsBaseArgs, BaseArgs, Args);
+            }
+
+            if (name.EndsWith(Base, StringComparison.Ordinal))
+            {
+                return GetNameWithoutSuffixes(name, EventArgBase, EventsArgBase, EventArgsBase, EventsArgsBase, Base);
+            }
+
+            return name;
+        }
+
+        private static string GetNameWithoutSuffixes(string name, params string[] phrases)
+        {
+            foreach (var phrase in phrases)
+            {
+                if (name.EndsWith(phrase, StringComparison.Ordinal))
                 {
-                    return name.WithoutSuffix(EventsArg);
+                    return name.WithoutSuffix(phrase);
                 }
             }
 
