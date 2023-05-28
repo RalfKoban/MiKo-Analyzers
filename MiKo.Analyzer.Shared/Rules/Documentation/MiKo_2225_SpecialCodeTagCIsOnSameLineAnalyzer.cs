9b14b135441a34c65a60b6a446c2cee078e9d6e7
Koban, Ralf <rkn@mm-software.com>
2023-05-28 11:33:55 +0000

    MiKo_2225 refactored to inspect only XmlElementSyntax nodes

--- MiKo.Analyzer.Shared/Rules/Documentation/MiKo_2225_SpecialCodeTagCIsOnSameLineAnalyzer.cs
+++ MiKo.Analyzer.Shared/Rules/Documentation/MiKo_2225_SpecialCodeTagCIsOnSameLineAnalyzer.cs
@@ -1,33 +1,33 @@
-﻿using System.Collections.Generic;
-
-using Microsoft.CodeAnalysis;
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
 namespace MiKoSolutions.Analyzers.Rules.Documentation
 {
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
-    public sealed class MiKo_2225_SpecialCodeTagCIsOnSameLineAnalyzer : OverallDocumentationAnalyzer
+    public sealed class MiKo_2225_SpecialCodeTagCIsOnSameLineAnalyzer : DocumentationAnalyzer
     {
         public const string Id = "MiKo_2225";
 
-        public MiKo_2225_SpecialCodeTagCIsOnSameLineAnalyzer() : base(Id)
+        public MiKo_2225_SpecialCodeTagCIsOnSameLineAnalyzer() : base(Id, (SymbolKind)(-1))
         {
         }
 
-        protected override IEnumerable<Diagnostic> AnalyzeComment(ISymbol symbol, Compilation compilation, string commentXml, DocumentationCommentTriviaSyntax comment)
-        {
-            foreach (var node in comment.DescendantNodes<XmlElementSyntax>())
-            {
-                if (node.GetXmlTagName() == Constants.XmlTag.C)
-                {
-                    var start = node.StartTag.GetStartingLine();
-                    var end = node.EndTag.GetStartingLine();
+        protected override void InitializeCore(CompilationStartAnalysisContext context) => context.RegisterSyntaxNodeAction(AnalyzeXmlElement, SyntaxKind.XmlElement);
 
-                    if (start != end)
-                    {
-                        yield return Issue(node);
-                    }
+        private void AnalyzeXmlElement(SyntaxNodeAnalysisContext context)
+        {
+            var node = (XmlElementSyntax)context.Node;
+
+            if (node.GetXmlTagName() == Constants.XmlTag.C)
+            {
+                var start = node.StartTag.GetStartingLine();
+                var end = node.EndTag.GetStartingLine();
+
+                if (start != end)
+                {
+                    ReportDiagnostics(context, Issue(node));
                 }
             }
         }
