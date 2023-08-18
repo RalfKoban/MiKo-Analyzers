09c2aa644c32ebcfd75ca73b1c928ee9cebb9ecd
Ralf Koban <r.kn@gmx.net>
2023-08-18 06:35:11 +0000

    [Isolated] attribute removed from tests so that they can now run together with other tests

--- MiKo.Analyzer.Tests/Rules/Documentation/MiKo_2012_MeaninglessSummaryAnalyzerTests.cs
+++ MiKo.Analyzer.Tests/Rules/Documentation/MiKo_2012_MeaninglessSummaryAnalyzerTests.cs
@@ -5,15 +5,13 @@ using System.Linq;
 using Microsoft.CodeAnalysis.CodeFixes;
 using Microsoft.CodeAnalysis.Diagnostics;
 
-using NCrunch.Framework;
-
 using NUnit.Framework;
 
 using TestHelper;
 
 namespace MiKoSolutions.Analyzers.Rules.Documentation
 {
-    [TestFixture, Isolated]
+    [TestFixture]
     public sealed class MiKo_2012_MeaninglessSummaryAnalyzerTests : CodeFixVerifier
     {
         private static readonly string[] MeaninglessTextPhrases =
