49272517d51a86fbc31233859e65e71a4831f8c4
Ralf Koban <r.kn@gmx.net>
2023-10-03 15:16:56 +0000

    Added new rule MiKo_6048 that reports for complex logical conditions to be on same line (resolves #555)

---
+++ MiKo.Analyzer.Shared/Rules/Spacing/MiKo_6048_LogicalConditionsAreOnSameLineAnalyzer.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace MiKoSolutions.Analyzers.Rules.Spacing
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class MiKo_6048_LogicalConditionsAreOnSameLineAnalyzer : SpacingAnalyzer
+    {
+        public const string Id = "MiKo_6048";
+
+        private static readonly SyntaxKind[] Expressions = { SyntaxKind.LogicalAndExpression, SyntaxKind.LogicalOrExpression };
+
+        public MiKo_6048_LogicalConditionsAreOnSameLineAnalyzer() : base(Id)
+        {
+        }
+
+        protected override void InitializeCore(CompilationStartAnalysisContext context) => context.RegisterSyntaxNodeAction(AnalyzeNode, Expressions);
+
+        private static bool IsOnSingleLine(SyntaxNode syntax)
+        {
+            while (true)
+            {
+                if (syntax is BinaryExpressionSyntax)
+                {
+                    return true; // ignore combined binaries
+                }
+
+                if (syntax is ParenthesizedExpressionSyntax parenthesized)
+                {
+                    syntax = parenthesized.Expression;
+
+                    continue;
+                }
+
+                var span = syntax.GetLocation().GetLineSpan();
+
+                return span.StartLinePosition.Line == span.EndLinePosition.Line;
+            }
+        }
+
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            if (context.Node is BinaryExpressionSyntax syntax)
+            {
+                ReportDiagnostics(context, AnalyzeNode(syntax));
+            }
+        }
+
+        private IEnumerable<Diagnostic> AnalyzeNode(BinaryExpressionSyntax syntax)
+        {
+            if (IsOnSingleLine(syntax.Left) is false)
+            {
+                yield return Issue(syntax.Left);
+            }
+
+            if (IsOnSingleLine(syntax.Right) is false)
+            {
+                yield return Issue(syntax.Right);
+            }
+        }
+    }
+}
---
+++ MiKo.Analyzer.Tests/Rules/Spacing/MiKo_6048_LogicalConditionsAreOnSameLineAnalyzerTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+
+using NUnit.Framework;
+
+using TestHelper;
+
+namespace MiKoSolutions.Analyzers.Rules.Spacing
+{
+    [TestFixture]
+    public sealed class MiKo_6048_LogicalConditionsAreOnSameLineAnalyzerTests : CodeFixVerifier
+    {
+        [Test]
+        public void No_issue_is_reported_if_logical_condition_is_on_same_line() => No_issue_is_reported_for(@"
+using System;
+
+public class TestMe
+{
+    public void DoSomething(bool flag1, bool flag2)
+    {
+        if (flag1 || flag2)
+            return;
+    }
+}
+");
+
+        protected override string GetDiagnosticId() => MiKo_6048_LogicalConditionsAreOnSameLineAnalyzer.Id;
+
+        protected override DiagnosticAnalyzer GetObjectUnderTest() => new MiKo_6048_LogicalConditionsAreOnSameLineAnalyzer();
+    }
+}
