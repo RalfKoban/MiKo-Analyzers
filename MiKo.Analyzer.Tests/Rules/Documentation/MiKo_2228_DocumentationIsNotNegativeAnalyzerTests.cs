e4da5dc4754f0349b36eb8b07cb9e85522b3214e
Ralf Koban <r.kn@gmx.net>
2023-10-23 10:54:10 +0000

    Added tests and some more wordings for MiKo_2228

--- MiKo.Analyzer.Tests/Rules/Documentation/MiKo_2228_DocumentationIsNotNegativeAnalyzerTests.cs
+++ MiKo.Analyzer.Tests/Rules/Documentation/MiKo_2228_DocumentationIsNotNegativeAnalyzerTests.cs
@@ -9,6 +9,64 @@ namespace MiKoSolutions.Analyzers.Rules.Documentation
     [TestFixture]
     public sealed class MiKo_2228_DocumentationIsNotNegativeAnalyzerTests : CodeFixVerifier
     {
+        [Test]
+        public void No_issue_is_reported_for_undocumented_method() => No_issue_is_reported_for(@"
+public class TestMe
+{
+    public void DoSomething()
+    {
+    }
+}
+");
+
+        [TestCase("Can be invoked.")]
+        [TestCase("Cannot be invoked.")]
+        public void No_issue_is_reported_for_method_with_documentation_(string text) => No_issue_is_reported_for(@"
+public class TestMe
+{
+    /// <summary>
+    /// " + text + @"
+    /// </summary>
+    public void DoSomething()
+    {
+    }
+}
+");
+
+        [TestCase("Shall not be invoked in case it is not available.")]
+        [TestCase("Should not be invoked in case it is not available.")]
+        [TestCase("Shouldn't be invoked in case it is not available.")]
+        [TestCase("Will not be invoked in case it is not available.")]
+        [TestCase("Won't be invoked in case it is not available.")]
+        [TestCase("Cannot be invoked in case it is not available.")]
+        [TestCase("Cannot be invoked if it isn't available.")]
+        [TestCase("Cannot be invoked if they aren't available.")]
+        [TestCase("Isn't available and cannot be invoked.")]
+        public void An_issue_is_reported_for_method_with_negative_documentation_(string text) => An_issue_is_reported_for(@"
+public class TestMe
+{
+    /// <summary>
+    /// " + text + @"
+    /// </summary>
+    public void DoSomething()
+    {
+    }
+}
+");
+
+        [TestCase("Isn't available and cannot be invoked.")]
+        public void An_issue_is_reported_for_method_with_negative_remarks_documentation_(string text) => An_issue_is_reported_for(@"
+public class TestMe
+{
+    /// <remarks>
+    /// " + text + @"
+    /// </remarks>
+    public void DoSomething()
+    {
+    }
+}
+");
+
         protected override string GetDiagnosticId() => MiKo_2228_DocumentationIsNotNegativeAnalyzer.Id;
 
         protected override DiagnosticAnalyzer GetObjectUnderTest() => new MiKo_2228_DocumentationIsNotNegativeAnalyzer();
