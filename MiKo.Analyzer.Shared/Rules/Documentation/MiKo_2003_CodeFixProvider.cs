7eb38ff60630def449bf563008db3c66ac090b60
Ralf Koban <r.kn@gmx.net>
2023-11-24 18:04:08 +0000

    MiKo_2003 can now detect and fix some more texts

--- MiKo.Analyzer.Shared/Rules/Documentation/MiKo_2003_CodeFixProvider.cs
+++ MiKo.Analyzer.Shared/Rules/Documentation/MiKo_2003_CodeFixProvider.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.Composition;
+using System.Linq;
 
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CodeFixes;
@@ -10,25 +11,46 @@ namespace MiKoSolutions.Analyzers.Rules.Documentation
     [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(MiKo_2003_CodeFixProvider)), Shared]
     public sealed class MiKo_2003_CodeFixProvider : SummaryDocumentationCodeFixProvider
     {
-        private static readonly Dictionary<string, string> ReplacementMap = new Dictionary<string, string>
-                                                                                {
-                                                                                    { "Called by the ", Constants.Comments.EventHandlerSummaryStartingPhrase },
-                                                                                    { "Called if the ", Constants.Comments.EventHandlerSummaryStartingPhrase },
-                                                                                    { "Called if ", Constants.Comments.EventHandlerSummaryStartingPhrase },
-                                                                                    { "Called when the ", Constants.Comments.EventHandlerSummaryStartingPhrase },
-                                                                                    { "Called when ", Constants.Comments.EventHandlerSummaryStartingPhrase },
-                                                                                    { "Callback that is called by the ", Constants.Comments.EventHandlerSummaryStartingPhrase },
-                                                                                    { "EventHandler for the ", Constants.Comments.EventHandlerSummaryStartingPhrase },
-                                                                                    { "EventHandler for ", Constants.Comments.EventHandlerSummaryStartingPhrase },
-                                                                                    { "Event handler for the ", Constants.Comments.EventHandlerSummaryStartingPhrase },
-                                                                                    { "Event handler for ", Constants.Comments.EventHandlerSummaryStartingPhrase },
-                                                                                    { "Handler for the ", Constants.Comments.EventHandlerSummaryStartingPhrase },
-                                                                                    { "Handler for ", Constants.Comments.EventHandlerSummaryStartingPhrase },
-                                                                                    { "Handle the ", Constants.Comments.EventHandlerSummaryStartingPhrase },
-                                                                                    { "Raised when the ", Constants.Comments.EventHandlerSummaryStartingPhrase },
-                                                                                    { "when the ", Constants.Comments.EventHandlerSummaryStartingPhrase },
-                                                                                    { "When the ", Constants.Comments.EventHandlerSummaryStartingPhrase },
-                                                                                };
+        private static readonly IReadOnlyCollection<string> ReplacementMapKeys = new[]
+                                                                                 {
+                                                                                     "Callback that is called by the ",
+                                                                                     "Called by ",
+                                                                                     "Called by the ",
+                                                                                     "Called if ",
+                                                                                     "Called if the ",
+                                                                                     "Called when ",
+                                                                                     "Called when the ",
+                                                                                     "Event handler for ",
+                                                                                     "Event Handler for ",
+                                                                                     "Event handler for the ",
+                                                                                     "Event Handler for the ",
+                                                                                     "Eventhandler for ",
+                                                                                     "EventHandler for ",
+                                                                                     "Eventhandler for the ",
+                                                                                     "EventHandler for the ",
+                                                                                     "Handle the ",
+                                                                                     "Handler for ",
+                                                                                     "Handler for the ",
+                                                                                     "Invoke the ",
+                                                                                     "Invoke the event ",
+                                                                                     "Invoked by ",
+                                                                                     "Invoked by the ",
+                                                                                     "Invoked by the event ",
+                                                                                     "Invoked when ",
+                                                                                     "Invoked when the ",
+                                                                                     "Invoked when the event ",
+                                                                                     "Raised by ",
+                                                                                     "Raised by the ",
+                                                                                     "Raised when ",
+                                                                                     "Raised when the ",
+                                                                                     "when the ",
+                                                                                     "When the ",
+                                                                                 };
+
+        private static readonly IReadOnlyCollection<KeyValuePair<string, string>> ReplacementMap = ReplacementMapKeys.OrderByDescending(_ => _.Length)
+                                                                                                                     .ThenBy(_ => _)
+                                                                                                                     .Select(_ => new KeyValuePair<string, string>(_, Constants.Comments.EventHandlerSummaryStartingPhrase))
+                                                                                                                     .ToArray();
 
         public override string FixableDiagnosticId => MiKo_2003_EventHandlerSummaryAnalyzer.Id;
 
@@ -38,7 +60,7 @@ namespace MiKoSolutions.Analyzers.Rules.Documentation
         {
             var comment = (XmlElementSyntax)syntax;
 
-            return Comment(comment, ReplacementMap.Keys, ReplacementMap);
+            return Comment(comment, ReplacementMapKeys, ReplacementMap);
         }
     }
 }
