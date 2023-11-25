7fa7eae732fe79ef33eee69498ed5c759e75afa5
Ralf Koban <r.kn@gmx.net>
2023-11-25 09:40:29 +0000

    Added new rule MiKo_6051 that reports and fixes colon tokens of ctor initializer calls
    (resolves #689)

---
+++ MiKo.Analyzer.Shared/Rules/Spacing/MiKo_6051_ConstructorOperatorsAreOnSameLineAsRightArgumentsAnalyzer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace MiKoSolutions.Analyzers.Rules.Spacing
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class MiKo_6051_ConstructorOperatorsAreOnSameLineAsRightArgumentsAnalyzer : SpacingAnalyzer
+    {
+        public const string Id = "MiKo_6051";
+
+        private const string Spaces = "SPACES";
+
+        public MiKo_6051_ConstructorOperatorsAreOnSameLineAsRightArgumentsAnalyzer() : base(Id)
+        {
+        }
+
+        internal static int GetSpaces(Diagnostic diagnostic) => int.Parse(diagnostic.Properties[Spaces]);
+
+        protected override void InitializeCore(CompilationStartAnalysisContext context) => context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.BaseConstructorInitializer, SyntaxKind.ThisConstructorInitializer);
+
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var node = (ConstructorInitializerSyntax)context.Node;
+
+            var startLine = node.ColonToken.GetStartingLine();
+            var rightPosition = node.ThisOrBaseKeyword.GetStartPosition();
+
+            if (startLine != rightPosition.Line)
+            {
+                ReportDiagnostics(context, Issue(node.ColonToken, new Dictionary<string, string> { { Spaces, rightPosition.Character.ToString("D") } }));
+            }
+        }
+    }
+}
