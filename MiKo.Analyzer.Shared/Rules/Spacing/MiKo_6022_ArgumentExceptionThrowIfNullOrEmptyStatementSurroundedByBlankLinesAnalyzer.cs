a6fa7f1c0498ae30167eb494cb9c05ac0f326980
Ralf Koban <r.kn@gmx.net>
2024-07-09 11:16:10 +0000

    typo 'eg.' fixed to 'e.g.'

--- MiKo.Analyzer.Shared/Rules/Spacing/MiKo_6022_ArgumentExceptionThrowIfNullOrEmptyStatementSurroundedByBlankLinesAnalyzer.cs
+++ MiKo.Analyzer.Shared/Rules/Spacing/MiKo_6022_ArgumentExceptionThrowIfNullOrEmptyStatementSurroundedByBlankLinesAnalyzer.cs
@@ -17,7 +17,7 @@ namespace MiKoSolutions.Analyzers.Rules.Spacing
 
         protected override bool IsApplicable(CompilationStartAnalysisContext context) => context.Compilation.GetTypeByMetadataName("System.ArgumentException") != null;
 
-        // it may happen that in some broken code Roslyn is unable to detect a type (eg. due to missing code paths), hence 'type' could be null here
+        // it may happen that in some broken code Roslyn is unable to detect a type (e.g. due to missing code paths), hence 'type' could be null here
         protected override bool IsCall(ITypeSymbol type) => type?.Name == nameof(ArgumentException);
 
         protected override bool IsCall(MemberAccessExpressionSyntax call, SemanticModel semanticModel) => call.GetName() == "ThrowIfNullOrEmpty" && base.IsCall(call, semanticModel);
