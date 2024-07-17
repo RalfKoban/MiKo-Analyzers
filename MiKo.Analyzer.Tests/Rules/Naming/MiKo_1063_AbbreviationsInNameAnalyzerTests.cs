34022e28fd583159c90da2b02dbca8a021d1ed7e
Ralf Koban <r.kn@gmx.net>
2024-07-17 14:52:18 +0000

    MiKo_1063 is now aware of abbreviations 'encr' and 'decr'

--- MiKo.Analyzer.Tests/Rules/Naming/MiKo_1063_AbbreviationsInNameAnalyzerTests.cs
+++ MiKo.Analyzer.Tests/Rules/Naming/MiKo_1063_AbbreviationsInNameAnalyzerTests.cs
@@ -36,6 +36,7 @@ namespace MiKoSolutions.Analyzers.Rules.Naming
                                                            "db",
                                                            "ddl",
                                                            "decl",
+                                                           "decr",
                                                            "desc",
                                                            "dest",
                                                            "diag",
@@ -49,6 +50,7 @@ namespace MiKoSolutions.Analyzers.Rules.Naming
                                                            "dst",
                                                            "dto",
                                                            "ef",
+                                                           "encr",
                                                            "env",
                                                            "environ",
                                                            "err",
@@ -122,6 +124,7 @@ namespace MiKoSolutions.Analyzers.Rules.Naming
                                                            "Db",
                                                            "Ddl",
                                                            "Decl",
+                                                           "Decr",
                                                            "Desc",
                                                            "Dest",
                                                            "Diag",
@@ -134,6 +137,7 @@ namespace MiKoSolutions.Analyzers.Rules.Naming
                                                            "Doc",
                                                            "Dst",
                                                            "Ef",
+                                                           "Encr",
                                                            "Env",
                                                            "Environ",
                                                            "Err",
@@ -294,63 +298,6 @@ namespace Bla
     }
 }
 ");
-
-        [Test]
-        public void No_issue_is_reported_for_Json_constructor_([ValueSource(nameof(BadPrefixes))] string name) => No_issue_is_reported_for(@"
-using System;
-using System.Text.Json.Serialization;
-
-namespace Bla
-{
-    public class TestMe
-    {
-        [JsonConstructor]
-        public TestMe(int " + name + @")
-        {
-        }
-    }
-}
-");
-
-        [Test]
-        public void No_issue_is_reported_for_Newtonsoft_Json_constructor_([ValueSource(nameof(BadPrefixes))] string name) => No_issue_is_reported_for(@"
-using System;
-
-namespace Bla
-{
-    public class TestMe
-    {
-        [Newtonsoft.Json.JsonConstructorAttribute]
-        public TestMe(int " + name + @")
-        {
-        }
-    }
-}
-");
-
-        [Test]
-        public void No_issue_is_reported_for_extern_method_([ValueSource(nameof(BadPrefixes))] string part) => No_issue_is_reported_for(@"
-using System;
-
-namespace Bla
-{
-    public class TestMe
-    {
-        public static extern int " + part + @"DoSomething();
-    }
-}");
-
-        [Test]
-        public void No_issue_is_reported_for_parameters_of_extern_method_([ValueSource(nameof(BadPrefixes))] string part) => No_issue_is_reported_for(@"
-using System;
-
-namespace Bla
-{
-    public class TestMe
-    {
-        public static extern int DoSomething(int " + part + @"Parameter);
-    }
-}");
 
         [Test]
         public void No_issue_is_reported_for_well_known_abbreviation_([Values("MEF")] string abbreviation) => No_issue_is_reported_for(@"
