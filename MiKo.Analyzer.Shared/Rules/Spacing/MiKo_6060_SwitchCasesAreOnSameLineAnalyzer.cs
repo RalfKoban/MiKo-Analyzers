0e17b57ec1d4b0ec8827d0f0a43e000f1f40d976
Ralf Koban <r.kn@gmx.net>
2024-10-04 09:22:07 +0000

    Added new rule MiKo_6060 for switch casel labels to be on single line
    (resolves #1059)

---
+++ MiKo.Analyzer.Shared/Rules/Spacing/MiKo_6060_SwitchCasesAreOnSameLineAnalyzer.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace MiKoSolutions.Analyzers.Rules.Spacing
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class MiKo_6060_SwitchCasesAreOnSameLineAnalyzer : SpacingAnalyzer
+    {
+        public const string Id = "MiKo_6060";
+
+        private static readonly SyntaxKind[] SwitchLabels = { SyntaxKind.CaseSwitchLabel, SyntaxKind.CasePatternSwitchLabel };
+
+        public MiKo_6060_SwitchCasesAreOnSameLineAnalyzer() : base(Id)
+        {
+        }
+
+        protected override void InitializeCore(CompilationStartAnalysisContext context) => context.RegisterSyntaxNodeAction(AnalyzeNode, SwitchLabels);
+
+        private static bool HasIssue(SyntaxNode node)
+        {
+            switch (node)
+            {
+                case CaseSwitchLabelSyntax label:
+                    return label.IsSpanningMultipleLines();
+
+                case CasePatternSwitchLabelSyntax pattern:
+                {
+                    if (pattern.WhenClause?.Condition is BinaryExpressionSyntax condition)
+                    {
+                        if (condition.Left is BinaryExpressionSyntax || condition.Right is BinaryExpressionSyntax)
+                        {
+                            return false;
+                        }
+                    }
+
+                    return pattern.IsSpanningMultipleLines();
+                }
+
+                default:
+                    return false;
+            }
+        }
+
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            var node = context.Node;
+
+            if (HasIssue(node))
+            {
+                ReportDiagnostics(context, Issue(node));
+            }
+        }
+    }
+}
