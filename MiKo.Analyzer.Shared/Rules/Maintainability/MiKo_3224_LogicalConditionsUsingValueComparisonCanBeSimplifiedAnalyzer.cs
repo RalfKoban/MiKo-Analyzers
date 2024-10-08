9360d366cc384165008d2ada686bd20ca65da2f7
Ralf Koban <r.kn@gmx.net>
2024-10-08 12:05:00 +0000

    Added new rule MiKo_3224 to simplify vaöue comparisons

---
+++ MiKo.Analyzer.Shared/Rules/Maintainability/MiKo_3224_LogicalConditionsUsingValueComparisonCanBeSimplifiedAnalyzer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace MiKoSolutions.Analyzers.Rules.Maintainability
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class MiKo_3224_LogicalConditionsUsingValueComparisonCanBeSimplifiedAnalyzer : LogicalConditionsComparisonCanBeSimplifiedMaintainabilityAnalyzer
+    {
+        public const string Id = "MiKo_3224";
+
+        public MiKo_3224_LogicalConditionsUsingValueComparisonCanBeSimplifiedAnalyzer() : base(Id)
+        {
+        }
+
+        protected override bool IsApplicable(IReadOnlyList<ArgumentSyntax> arguments) => arguments.Count == 1;
+
+        protected override bool IsApplicable(ITypeSymbol typeSymbol) => typeSymbol.IsValueType;
+    }
+}
