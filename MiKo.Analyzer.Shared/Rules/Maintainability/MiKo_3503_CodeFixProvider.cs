40650943e02b848e90f173f0bf2bdb5deed98388
Ralf Koban <r.kn@gmx.net>
2025-02-20 18:06:25 +0000

    Added new rule MiKo_3503 that reports when returning variables immediately after assigned within try/catch blocks
    (resolves #1170)

---
+++ MiKo.Analyzer.Shared/Rules/Maintainability/MiKo_3503_CodeFixProvider.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MiKoSolutions.Analyzers.Rules.Maintainability
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(MiKo_3503_CodeFixProvider)), Shared]
+    public sealed class MiKo_3503_CodeFixProvider : MaintainabilityCodeFixProvider
+    {
+        public override string FixableDiagnosticId => "MiKo_3503";
+
+        protected override SyntaxNode GetSyntax(IEnumerable<SyntaxNode> syntaxNodes) => syntaxNodes.OfType<ReturnStatementSyntax>().FirstOrDefault();
+
+        protected override SyntaxNode GetUpdatedSyntax(Document document, SyntaxNode syntax, Diagnostic issue) => syntax;
+
+        protected override SyntaxNode GetUpdatedSyntaxRoot(Document document, SyntaxNode root, SyntaxNode syntax, SyntaxAnnotation annotationOfSyntax, Diagnostic issue)
+        {
+            if (syntax is ReturnStatementSyntax returnStatement
+             && returnStatement.PreviousSibling() is TryStatementSyntax tryStatement
+             && tryStatement.PreviousSibling() is LocalDeclarationStatementSyntax localDeclarationStatement)
+            {
+                return root.ReplaceNodes(
+                                     new SyntaxNode[] { localDeclarationStatement, tryStatement, returnStatement },
+                                     (original, rewritten) =>
+                                                              {
+                                                                  if (rewritten == returnStatement)
+                                                                  {
+                                                                      return null; // remove the return statement
+                                                                  }
+
+                                                                  if (rewritten == localDeclarationStatement)
+                                                                  {
+                                                                      return null; // remove the variable declaration statement
+                                                                  }
+
+                                                                  if (rewritten is TryStatementSyntax statement)
+                                                                  {
+                                                                      // move the statements inside the try block
+                                                                      var block = statement.Block;
+                                                                      var statements = block.Statements
+                                                                                            .Insert(0, localDeclarationStatement.WithAdditionalLeadingSpaces(Constants.Indentation))
+                                                                                            .Add(returnStatement.WithAdditionalLeadingSpaces(Constants.Indentation));
+
+                                                                      return statement.WithBlock(block.WithStatements(statements));
+                                                                  }
+
+                                                                  return rewritten;
+                                                              });
+            }
+
+            return null;
+        }
+    }
+}
---
+++ MiKo.Analyzer.Shared/Rules/Maintainability/MiKo_3503_DoNotReturnVariableImmediatelyAfterTryCatchBlockAnalyzer.cs
@@ -0,0 +1,67 @@
+﻿using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace MiKoSolutions.Analyzers.Rules.Maintainability
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class MiKo_3503_DoNotReturnVariableImmediatelyAfterTryCatchBlockAnalyzer : MaintainabilityAnalyzer
+    {
+        public const string Id = "MiKo_3503";
+
+        public MiKo_3503_DoNotReturnVariableImmediatelyAfterTryCatchBlockAnalyzer() : base(Id)
+        {
+        }
+
+        protected override void InitializeCore(CompilationStartAnalysisContext context) => context.RegisterSyntaxNodeAction(AnalyzeTryStatement, SyntaxKind.TryStatement);
+
+        private void AnalyzeTryStatement(SyntaxNodeAnalysisContext context)
+        {
+            if (context.Node is TryStatementSyntax node)
+            {
+                var issue = Analyze(node);
+
+                if (issue != null)
+                {
+                    context.ReportDiagnostic(issue);
+                }
+            }
+        }
+
+        private Diagnostic Analyze(TryStatementSyntax tryCatchBlock)
+        {
+            if (tryCatchBlock.PreviousSibling() is LocalDeclarationStatementSyntax declaration)
+            {
+                if (tryCatchBlock.NextSibling() is ReturnStatementSyntax returnStatement && returnStatement.Expression is IdentifierNameSyntax identifier)
+                {
+                    var identifierName = identifier.GetName();
+
+                    if (declaration.Declaration.Variables.Any(_ => _.GetName() == identifierName))
+                    {
+                        var statements = tryCatchBlock.Block.Statements;
+                        var statementsCount = statements.Count;
+
+                        if (statementsCount > 0)
+                        {
+                            for (var index = 0; index < statementsCount; index++)
+                            {
+                                if (statements[index] is ExpressionStatementSyntax e && e.Expression is AssignmentExpressionSyntax assignment && assignment.IsKind(SyntaxKind.SimpleAssignmentExpression))
+                                {
+                                    if (assignment.Left is IdentifierNameSyntax left && left.GetName() == identifierName)
+                                    {
+                                        return Issue(identifier);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
