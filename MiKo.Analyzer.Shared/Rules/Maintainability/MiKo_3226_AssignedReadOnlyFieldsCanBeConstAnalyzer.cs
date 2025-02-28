290b7287bcfb5101e7a0a74cd40a54375d649a44
Ralf Koban <r.kn@gmx.net>
2025-02-28 17:35:47 +0000

    Added new rule MiKo_3226 that converts readonly fields into consts
    (resolves #1216)

---
+++ MiKo.Analyzer.Shared/Rules/Maintainability/MiKo_3226_AssignedReadOnlyFieldsCanBeConstAnalyzer.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace MiKoSolutions.Analyzers.Rules.Maintainability
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class MiKo_3226_AssignedReadOnlyFieldsCanBeConstAnalyzer : MaintainabilityAnalyzer
+    {
+        public const string Id = "MiKo_3226";
+
+        public MiKo_3226_AssignedReadOnlyFieldsCanBeConstAnalyzer() : base(Id, (SymbolKind)(-1))
+        {
+        }
+
+        protected override void InitializeCore(CompilationStartAnalysisContext context) => context.RegisterSyntaxNodeAction(AnalyzeField, SyntaxKind.FieldDeclaration);
+
+        private void AnalyzeField(SyntaxNodeAnalysisContext context)
+        {
+            if (context.Node is FieldDeclarationSyntax declaration && declaration.Modifiers.Any(SyntaxKind.ReadOnlyKeyword))
+            {
+                foreach (var variable in declaration.Declaration.Variables)
+                {
+                    if (variable.Initializer?.Value is LiteralExpressionSyntax)
+                    {
+                        ReportDiagnostics(context, Issue(declaration));
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ MiKo.Analyzer.Shared/Rules/Maintainability/MiKo_3226_CodeFixProvider.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MiKoSolutions.Analyzers.Rules.Maintainability
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(MiKo_3226_CodeFixProvider)), Shared]
+    public sealed class MiKo_3226_CodeFixProvider : MaintainabilityCodeFixProvider
+    {
+        public override string FixableDiagnosticId => "MiKo_3226";
+
+        protected override SyntaxNode GetSyntax(IEnumerable<SyntaxNode> syntaxNodes) => syntaxNodes.OfType<FieldDeclarationSyntax>().FirstOrDefault();
+
+        protected override SyntaxNode GetUpdatedSyntax(Document document, SyntaxNode syntax, Diagnostic issue)
+        {
+            if (syntax is FieldDeclarationSyntax field)
+            {
+                var modifiers = field.Modifiers;
+                var readonlyModifier = modifiers.First(SyntaxKind.ReadOnlyKeyword);
+                var updatedModifiers = modifiers.Replace(readonlyModifier, SyntaxKind.ConstKeyword.AsToken());
+
+                var staticModifier = updatedModifiers.First(SyntaxKind.StaticKeyword);
+
+                return field.WithModifiers(staticModifier.IsDefaultValue() ? updatedModifiers : updatedModifiers.Remove(staticModifier));
+            }
+
+            return syntax;
+        }
+    }
+}
