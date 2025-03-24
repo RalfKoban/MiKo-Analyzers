cbf58551c2b5e51f3e2dd094293c2a6e37f624bb
Ralf Koban <r.kn@gmx.net>
2025-03-24 15:02:31 +0000

    Added new rule MiKo_6063 that reports if invocation is placed on another line and provides fix to place it on same line
    (resolves #1209)

---
+++ MiKo.Analyzer.Shared/Rules/Spacing/MiKo_6063_CodeFixProvider.cs
@@ -0,0 +1,24 @@
+﻿using System.Composition;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MiKoSolutions.Analyzers.Rules.Spacing
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(MiKo_6063_CodeFixProvider)), Shared]
+    public sealed class MiKo_6063_CodeFixProvider : SpacingCodeFixProvider
+    {
+        public override string FixableDiagnosticId => "MiKo_6063";
+
+        protected override SyntaxNode GetUpdatedSyntax(Document document, SyntaxNode syntax, Diagnostic issue)
+        {
+            if (syntax is InvocationExpressionSyntax invocation)
+            {
+                return invocation.WithExpression(PlacedOnSameLine(invocation.Expression));
+            }
+
+            return syntax;
+        }
+    }
+}
