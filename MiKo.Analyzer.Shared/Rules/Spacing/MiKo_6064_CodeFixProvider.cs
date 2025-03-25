67da96d0ea87b6aa91a92d89d763fca5710eeb6e
Ralf Koban <r.kn@gmx.net>
2025-03-25 12:45:38 +0000

    Added new rule MiKo_6064 for identifier call chains to be placed on same line
    (resolves #1267)

---
+++ MiKo.Analyzer.Shared/Rules/Spacing/MiKo_6064_CodeFixProvider.cs
@@ -0,0 +1,24 @@
+﻿using System.Composition;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MiKoSolutions.Analyzers.Rules.Spacing
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(MiKo_6064_CodeFixProvider)), Shared]
+    public sealed class MiKo_6064_CodeFixProvider : SpacingCodeFixProvider
+    {
+        public override string FixableDiagnosticId => "MiKo_6064";
+
+        protected override SyntaxNode GetUpdatedSyntax(Document document, SyntaxNode syntax, Diagnostic issue)
+        {
+            if (syntax is MemberAccessExpressionSyntax member)
+            {
+                return PlacedOnSameLine(member).WithTriviaFrom(member);
+            }
+
+            return syntax;
+        }
+    }
+}
---
+++ MiKo.Analyzer.Shared/Rules/Spacing/MiKo_6064_PropertyInvocationIsOnSameLineAnalyzer.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace MiKoSolutions.Analyzers.Rules.Spacing
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public class MiKo_6064_PropertyInvocationIsOnSameLineAnalyzer : SpacingAnalyzer
+    {
+        public const string Id = "MiKo_6064";
+
+        public MiKo_6064_PropertyInvocationIsOnSameLineAnalyzer() : base(Id)
+        {
+        }
+
+        protected override void InitializeCore(CompilationStartAnalysisContext context) => context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.SimpleMemberAccessExpression);
+
+        private static bool HasIssue(MemberAccessExpressionSyntax member)
+        {
+            if (member.IsKind(SyntaxKind.SimpleMemberAccessExpression) && member.Name is IdentifierNameSyntax name && member.Expression is IdentifierNameSyntax expression)
+            {
+                if (name.GetStartingLine() != expression.GetStartingLine())
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void AnalyzeNode(SyntaxNodeAnalysisContext context)
+        {
+            if (context.Node is MemberAccessExpressionSyntax member && HasIssue(member))
+            {
+                ReportDiagnostics(context, Issue(member));
+            }
+        }
+    }
+}
