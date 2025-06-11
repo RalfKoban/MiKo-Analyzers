0ec1bafdf436c5092c61efc5c21d4479c0396a11
Ralf Koban <r.kn@gmx.net>
2025-06-11 08:49:41 +0000

    Code extracted into own methods

--- MiKo.Analyzer.Shared/Rules/Spacing/MiKo_6030_CodeFixProvider.cs
+++ MiKo.Analyzer.Shared/Rules/Spacing/MiKo_6030_CodeFixProvider.cs
@@ -19,31 +19,41 @@ namespace MiKoSolutions.Analyzers.Rules.Spacing
             {
                 case InitializerExpressionSyntax initializer:
                 {
-                    var openBraceToken = initializer.OpenBraceToken;
-                    var closeBraceToken = initializer.CloseBraceToken;
-
-                    var closeBraceTokenSpaces = openBraceToken.IsOnSameLineAs(closeBraceToken) ? 0 : spaces;
-
-                    return initializer.WithOpenBraceToken(openBraceToken.WithLeadingSpaces(spaces))
-                                      .WithExpressions(GetUpdatedSyntax(initializer.Expressions, openBraceToken, spaces + Constants.Indentation))
-                                      .WithCloseBraceToken(closeBraceToken.WithLeadingSpaces(closeBraceTokenSpaces));
+                    return GetUpdatedSyntax(initializer, spaces);
                 }
 
                 case AnonymousObjectCreationExpressionSyntax anonymous:
                 {
-                    var openBraceToken = anonymous.OpenBraceToken;
-                    var closeBraceToken = anonymous.CloseBraceToken;
-
-                    var closeBraceTokenSpaces = openBraceToken.IsOnSameLineAs(closeBraceToken) ? 0 : spaces;
-
-                    return anonymous.WithOpenBraceToken(openBraceToken.WithLeadingSpaces(spaces))
-                                    .WithInitializers(GetUpdatedSyntax(anonymous.Initializers, openBraceToken, spaces + Constants.Indentation))
-                                    .WithCloseBraceToken(closeBraceToken.WithLeadingSpaces(closeBraceTokenSpaces));
+                    return GetUpdatedSyntax(anonymous, spaces);
                 }
 
                 default:
                     return syntax;
             }
+        }
+
+        private static AnonymousObjectCreationExpressionSyntax GetUpdatedSyntax(AnonymousObjectCreationExpressionSyntax anonymous, int spaces)
+        {
+            var openBraceToken = anonymous.OpenBraceToken;
+            var closeBraceToken = anonymous.CloseBraceToken;
+
+            var closeBraceTokenSpaces = openBraceToken.IsOnSameLineAs(closeBraceToken) ? 0 : spaces;
+
+            return anonymous.WithOpenBraceToken(openBraceToken.WithLeadingSpaces(spaces))
+                            .WithInitializers(GetUpdatedSyntax(anonymous.Initializers, openBraceToken, spaces + Constants.Indentation))
+                            .WithCloseBraceToken(closeBraceToken.WithLeadingSpaces(closeBraceTokenSpaces));
+        }
+
+        private static InitializerExpressionSyntax GetUpdatedSyntax(InitializerExpressionSyntax initializer, int spaces)
+        {
+            var openBraceToken = initializer.OpenBraceToken;
+            var closeBraceToken = initializer.CloseBraceToken;
+
+            var closeBraceTokenSpaces = openBraceToken.IsOnSameLineAs(closeBraceToken) ? 0 : spaces;
+
+            return initializer.WithOpenBraceToken(openBraceToken.WithLeadingSpaces(spaces))
+                              .WithExpressions(GetUpdatedSyntax(initializer.Expressions, openBraceToken, spaces + Constants.Indentation))
+                              .WithCloseBraceToken(closeBraceToken.WithLeadingSpaces(closeBraceTokenSpaces));
         }
     }
 }
