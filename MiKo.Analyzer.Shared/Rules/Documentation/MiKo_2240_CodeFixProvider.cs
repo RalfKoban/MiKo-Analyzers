b513040ad83e6d3a8353ca18ef36c283c11e5dd2
Ralf Koban <r.kn@gmx.net>
2025-07-05 11:31:06 +0000

    Added new rule MiKo_2240 that reports on <response> starting with 'Return' and provides a codefix
    (resolves #1397)

---
+++ MiKo.Analyzer.Shared/Rules/Documentation/MiKo_2240_CodeFixProvider.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MiKoSolutions.Analyzers.Rules.Documentation
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(MiKo_2240_CodeFixProvider)), Shared]
+    public sealed class MiKo_2240_CodeFixProvider : DocumentationCodeFixProvider
+    {
+        public override string FixableDiagnosticId => "MiKo_2240";
+
+        protected override SyntaxNode GetSyntax(IEnumerable<SyntaxNode> syntaxNodes) => syntaxNodes.OfType<XmlElementSyntax>().FirstOrDefault();
+
+        protected override SyntaxNode GetUpdatedSyntax(Document document, SyntaxNode syntax, Diagnostic issue)
+        {
+            if (syntax is XmlElementSyntax element)
+            {
+                var phrase = GetPhraseProposal(issue);
+
+                return Comment(element, phrase);
+            }
+
+            return syntax;
+        }
+    }
+}
