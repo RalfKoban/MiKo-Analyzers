65855dea534ee1ef0eb80ea4aa2bbff9a95072cf
Ralf Koban <r.kn@gmx.net>
2025-07-12 14:49:12 +0000

    MiKo_2100 is now aware of <example> documentation starting directly with <code> tags

--- MiKo.Analyzer.Shared/Rules/Documentation/MiKo_2100_CodeFixProvider.cs
+++ MiKo.Analyzer.Shared/Rules/Documentation/MiKo_2100_CodeFixProvider.cs
@@ -11,11 +11,42 @@ namespace MiKoSolutions.Analyzers.Rules.Documentation
     public sealed class MiKo_2100_CodeFixProvider : ExampleDocumentationCodeFixProvider
     {
         private const string Phrase = Constants.Comments.ExampleDefaultPhrase;
+        private const string CompletePhrase = Phrase + "its usage.";
 
         public override string FixableDiagnosticId => "MiKo_2100";
 
         protected override string Title => Resources.MiKo_2100_CodeFixTitle.FormatWith(Phrase);
 
-        protected override SyntaxNode GetUpdatedSyntax(Document document, SyntaxNode syntax, Diagnostic issue) => CommentStartingWith((XmlElementSyntax)syntax, Phrase);
+        protected override SyntaxNode GetUpdatedSyntax(Document document, SyntaxNode syntax, Diagnostic issue)
+        {
+            if (syntax is XmlElementSyntax element)
+            {
+                var content = element.Content;
+
+                if (content.Count > 0)
+                {
+                    if (content[0].IsCode())
+                    {
+                        var xmlText = XmlText(CompletePhrase).WithLeadingXmlComment()
+                                                             .WithTrailingNewLine()
+                                                             .WithTrailingXmlComment();
+
+                        return element.WithContent(element.Content.Insert(0, xmlText));
+                    }
+
+                    if (content[0].IsWhiteSpaceOnlyText() && content.Count > 1 && content[1].IsCode())
+                    {
+                        var xmlText = XmlText(CompletePhrase).WithTrailingNewLine()
+                                                             .WithTrailingXmlComment();
+
+                        return element.WithContent(element.Content.Insert(1, xmlText));
+                    }
+
+                    return CommentStartingWith(element, Phrase);
+                }
+            }
+
+            return syntax;
+        }
     }
 }
