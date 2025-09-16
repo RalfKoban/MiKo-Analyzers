2c8a29caf31b6658b8b2fae7b63a4b1d57d9e944
Ralf Koban <r.kn@gmx.net>
2025-09-16 14:35:59 +0000

    Added new rule MiKo_1514 that reports structural names in fields
    (resolves #1393)
    
    includes fixes for StringBuilderCache

---
+++ MiKo.Analyzer.Shared/Rules/Naming/MiKo_1514_FieldsWithStructuralDesignPatternSuffixAnalyzer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace MiKoSolutions.Analyzers.Rules.Naming
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class MiKo_1514_FieldsWithStructuralDesignPatternSuffixAnalyzer : NamingAnalyzer
+    {
+        public const string Id = "MiKo_1514";
+
+        public MiKo_1514_FieldsWithStructuralDesignPatternSuffixAnalyzer() : base(Id, SymbolKind.Field)
+        {
+        }
+
+        protected override bool ShallAnalyze(IFieldSymbol symbol) => IsNameForStructuralDesignPattern(symbol.Name);
+
+        protected override IEnumerable<Diagnostic> AnalyzeName(IFieldSymbol symbol, Compilation compilation)
+        {
+            var name = symbol.Name;
+            var prefix = GetFieldPrefix(name);
+            var betterName = FindBetterNameForStructuralDesignPattern(name, prefix);
+
+            return new[] { Issue(symbol, betterName, CreateBetterNameProposal(betterName)) };
+        }
+    }
+}
