82649aaf8c9d03dedd2b244364f466bb70ef28b2
Ralf Koban <r.kn@gmx.net>
2025-10-12 15:05:07 +0000

    Added rules MiKo_1520 and MiKo_1521 for local variables and parameters named 'toCopy'
    (resolves #1616)

---
+++ MiKo.Analyzer.Shared/Rules/Naming/MiKo_1520_CodeFixProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Composition;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+
+namespace MiKoSolutions.Analyzers.Rules.Naming
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(MiKo_1520_CodeFixProvider)), Shared]
+    public sealed class MiKo_1520_CodeFixProvider : LocalVariableNamingCodeFixProvider
+    {
+        public override string FixableDiagnosticId => "MiKo_1520";
+    }
+}
