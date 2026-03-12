fcb67810b75a5ad5e433e718182343c7fd4508fe
Ralf Koban <r.kn@gmx.net>
2026-03-12 12:03:16 +0000

    MiKo_2019 and MiKo_2070 are now aware of 'true only if'

--- MiKo.Analyzer.Shared/Rules/Documentation/MiKo_2012_CodeFixProvider.cs
+++ MiKo.Analyzer.Shared/Rules/Documentation/MiKo_2012_CodeFixProvider.cs
@@ -378,6 +378,10 @@ namespace MiKoSolutions.Analyzers.Rules.Documentation
             builder.ReplaceWithProbe(" tRUE if ", " whether ");
             builder.ReplaceWithProbe(" true when ", " whether ");
             builder.ReplaceWithProbe(" tRUE when ", " whether ");
+            builder.ReplaceWithProbe(" true only if ", " whether ");
+            builder.ReplaceWithProbe(" tRUE only if ", " whether ");
+            builder.ReplaceWithProbe(" true only when ", " whether ");
+            builder.ReplaceWithProbe(" tRUE only when ", " whether ");
             builder.ReplaceWithProbe(" when set to true then ", " whether ");
             builder.ReplaceWithProbe(" when set to true, then ", " whether ");
             builder.ReplaceWithProbe(" when set to True then ", " whether ");
