9969ba5d6f9209d5f7bc5a5951de8fd5b2e6c5cd
Ralf Koban <r.kn@gmx.net>
2026-04-30 15:16:45 +0000

    Added new rule MiKo_3233 to discourage 'is var' pattern for null checks
    - Introduce MiKo_3233 analyzer to detect and report usage of 'is var' patterns in null checks, preventing potential NullReferenceExceptions
    
    - Added analyzer implementation, unit tests, localized resources, and documentation
    
    - Updated README
    
    (resolves ##1977)

---
+++ MiKo.Analyzer.Shared/Rules/Maintainability/MiKo_3233_DoNotUseVarPatternInIsPatternAnalyzer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace MiKoSolutions.Analyzers.Rules.Maintainability
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class MiKo_3233_DoNotUseVarPatternInIsPatternAnalyzer : MaintainabilityAnalyzer
+    {
+        public const string Id = "MiKo_3233";
+
+        private static readonly SyntaxKind[] ParentKinds = { SyntaxKind.IsPatternExpression, SyntaxKind.NotPattern };
+
+        public MiKo_3233_DoNotUseVarPatternInIsPatternAnalyzer() : base(Id)
+        {
+        }
+
+        protected override void InitializeCore(CompilationStartAnalysisContext context) => context.RegisterSyntaxNodeAction(AnalyzeVarPattern, SyntaxKind.VarPattern);
+
+        private void AnalyzeVarPattern(SyntaxNodeAnalysisContext context)
+        {
+            if (context.Node is VarPatternSyntax pattern && pattern.Parent.IsAnyKind(ParentKinds))
+            {
+                ReportDiagnostics(context, Issue(pattern.VarKeyword));
+            }
+        }
+    }
+}
