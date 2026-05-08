1a3925cc4ba3a7802a7d1ca6672b86d5c4d30474
Ralf Koban <r.kn@gmx.net>
2026-05-08 08:22:43 +0000

    MiKo_2012 now attempts to underline the issue in the text and falls back to default if text is not directly found
    - Refactor MiKo_2012_MeaninglessSummaryAnalyzer to report diagnostics at precise XML locations
    
    - Add string-based `HumanizedTakeFirst` overload to `StringExtensions` and update usages to leverage the new overload for clarity
    
    - Introduce tests for `HumanizedTakeFirst`
    
    - Clarify documentation for location-returning methods in `DocumentationAnalyzer`
    
    (resolves #1596)

---
+++ MiKo.Analyzer.Tests/Extensions/StringExtensionsTests.HumanizedTakeFirst.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+//// ncrunch: rdi off
+namespace MiKoSolutions.Analyzers.Extensions
+{
+    [TestFixture]
+    public static partial class StringExtensionsTests
+    {
+        [Test]
+        public static void HumanizedTakeFirst_string_returns_empty_string_for_value() => Assert.That(((string)null).HumanizedTakeFirst(10), Is.Empty);
+
+        [TestCase("", 10, ExpectedResult = "")]
+        [TestCase("hello", 10, ExpectedResult = "hello")]
+        [TestCase("hello", 5, ExpectedResult = "hello")]
+        [TestCase("hello world", 5, ExpectedResult = "hello...")]
+        [TestCase("hello world", 0, ExpectedResult = "hello world")]
+        [TestCase("hello world", -1, ExpectedResult = "hello world")]
+        [TestCase("hello   ", 10, ExpectedResult = "hello")]
+        [TestCase("hello   ", 5, ExpectedResult = "hello")]
+        [TestCase("hello   ", 3, ExpectedResult = "hel...")]
+        [TestCase("hi  there", 4, ExpectedResult = "hi...")]
+        public static string HumanizedTakeFirst_string_returns_correct_result_(string value, int maximum) => value.HumanizedTakeFirst(maximum);
+
+        [TestCase("", 10, ExpectedResult = "")]
+        [TestCase("hello", 10, ExpectedResult = "hello")]
+        [TestCase("hello", 5, ExpectedResult = "hello")]
+        [TestCase("hello world", 5, ExpectedResult = "hello...")]
+        [TestCase("hello world", 0, ExpectedResult = "hello world")]
+        [TestCase("hello world", -1, ExpectedResult = "hello world")]
+        [TestCase("hello   ", 10, ExpectedResult = "hello")]
+        [TestCase("hello   ", 5, ExpectedResult = "hello")]
+        [TestCase("hello   ", 3, ExpectedResult = "hel...")]
+        [TestCase("hi  there", 4, ExpectedResult = "hi...")]
+        public static string HumanizedTakeFirst_ReadOnlySpan_returns_correct_result_(string value, int maximum) => ((System.ReadOnlySpan<char>)value).HumanizedTakeFirst(maximum);
+    }
+}
