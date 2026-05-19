fc471380cd0afba3e5d9d66d23c92b71b30a238b
Ralf Koban <r.kn@gmx.net>
2026-05-19 11:17:19 +0000

    Added new rule MiKo_1537 that reports methods starting with "Event"
    - Introduced analyzer and codefix to ensure methods are not prefixed with "Event" and instead use the "On" prefix for event handlers
    
    - Updated documentation, Readme.md and resources
    
    - Added unit tests to verify analyzer and code fix behavior
    
    (resolves #2009)

---
+++ MiKo.Analyzer.Shared/Rules/Naming/MiKo_1537_CodeFixProvider.cs
@@ -0,0 +1,13 @@
+﻿using System.Composition;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+
+namespace MiKoSolutions.Analyzers.Rules.Naming
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(MiKo_1537_CodeFixProvider)), Shared]
+    public sealed class MiKo_1537_CodeFixProvider : NamingCodeFixProvider
+    {
+        public override string FixableDiagnosticId => "MiKo_1537";
+    }
+}
