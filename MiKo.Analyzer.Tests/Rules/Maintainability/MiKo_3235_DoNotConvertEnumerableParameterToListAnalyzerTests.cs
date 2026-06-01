e0a7c227250abb62a572e2b67e74b7d8814fc1a4
Ralf Koban <r.kn@gmx.net>
2026-06-01 12:45:35 +0000

    Added new rule MiKo_3235 that reports 'ToList' calls on 'IEnumerable'
    - Add analyzer to detect and discourage `.ToList()` calls on `IEnumerable<T>` parameters, recommending `IReadOnlyList<T>` instead
    
    - Add unit tests
    
    - Update documentation, `README.md`, resources and project file
    
    (resolves #2038)

---
+++ MiKo.Analyzer.Tests/Rules/Maintainability/MiKo_3235_DoNotConvertEnumerableParameterToListAnalyzerTests.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.CodeAnalysis.Diagnostics;
+
+using NUnit.Framework;
+
+using TestHelper;
+
+//// ncrunch: rdi off
+namespace MiKoSolutions.Analyzers.Rules.Maintainability
+{
+    [TestFixture]
+    public sealed class MiKo_3235_DoNotConvertEnumerableParameterToListAnalyzerTests : CodeFixVerifier
+    {
+        [Test]
+        public void No_issue_is_reported_for_methods_with_no_IEnumerable_parameter() => No_issue_is_reported_for(@"
+using System;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public void DoSomething() { }
+    }
+}
+");
+
+        [Test]
+        public void No_issue_is_reported_for_methods_with_IEnumerable_parameter() => No_issue_is_reported_for(@"
+using System;
+using System.Collections.Generic;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public void DoSomething(IEnumerable<int> values)
+        {
+            foreach (var value in values)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
+");
+
+        [Test]
+        public void No_issue_is_reported_for_methods_with_IEnumerable_parameter_and_unrelated_ToList_call() => No_issue_is_reported_for(@"
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public List<int> DoSomething(IEnumerable<int> values)
+        {
+            List<int> results = new List<int>();
+
+            foreach (var value in values)
+            {
+                if (value > 42)
+                {
+                    results.Add(value);
+                }
+            }
+
+            return results.ToList();
+        }
+    }
+}
+");
+
+        [Test]
+        public void An_issue_is_reported_for_methods_with_IEnumerable_parameter_and_ToList_call_on_it() => An_issue_is_reported_for(@"
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public void DoSomething(IEnumerable<int> values)
+        {
+            var valuesList = values.ToList();
+
+            foreach (var value in valuesList)
+            {
+                Console.WriteLine(value);
+            }
+        }
+    }
+}
+");
+
+        protected override string GetDiagnosticId() => MiKo_3235_DoNotConvertEnumerableParameterToListAnalyzer.Id;
+
+        protected override DiagnosticAnalyzer GetObjectUnderTest() => new MiKo_3235_DoNotConvertEnumerableParameterToListAnalyzer();
+    }
+}
