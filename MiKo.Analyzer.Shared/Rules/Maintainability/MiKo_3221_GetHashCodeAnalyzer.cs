312e6a438a29a419e2519881f43c5285f4b277f5
Ralf Koban <r.kn@gmx.net>
2026-06-05 06:00:26 +0000

    removed unused using

--- MiKo.Analyzer.Shared/Rules/Maintainability/MiKo_3221_GetHashCodeAnalyzer.cs
+++ MiKo.Analyzer.Shared/Rules/Maintainability/MiKo_3221_GetHashCodeAnalyzer.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 
 using Microsoft.CodeAnalysis;
-using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Diagnostics;
 
