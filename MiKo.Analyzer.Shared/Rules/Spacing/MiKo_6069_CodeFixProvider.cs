f696da4c808c8bf9562d9aaf23c6261bb4d233a0
Ralf Koban <r.kn@gmx.net>
2026-06-24 13:03:55 +0000

    Added rule MiKo_6069 that reports and fixes object initializer expressions not positioned properly
    (resolves #2089)

---
+++ MiKo.Analyzer.Shared/Rules/Spacing/MiKo_6069_CodeFixProvider.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections.Generic;
+using System.Composition;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace MiKoSolutions.Analyzers.Rules.Spacing
+{
+    [ExportCodeFixProvider(LanguageNames.CSharp, Name = nameof(MiKo_6069_CodeFixProvider)), Shared]
+    public sealed class MiKo_6069_CodeFixProvider : SpacingCodeFixProvider
+    {
+        public override string FixableDiagnosticId => "MiKo_6069";
+
+        protected override SyntaxNode GetSyntax(IEnumerable<SyntaxNode> syntaxNodes) => syntaxNodes.OfType<AssignmentExpressionSyntax>().FirstOrDefault();
+
+        protected override Task<SyntaxNode> GetUpdatedSyntaxAsync(SyntaxNode syntax, Diagnostic issue, Document document, CancellationToken cancellationToken)
+        {
+            var updatedSyntax = GetUpdatedSyntax(syntax, issue);
+
+            return Task.FromResult(updatedSyntax);
+        }
+
+        protected override Task<SyntaxNode> GetUpdatedSyntaxRootAsync(Document document, SyntaxNode root, SyntaxNode syntax, SyntaxAnnotation annotationOfSyntax, Diagnostic issue, CancellationToken cancellationToken)
+        {
+            var updatedSyntax = GetUpdatedSyntaxRoot(root, syntax);
+
+            return Task.FromResult(updatedSyntax);
+        }
+
+        private static SyntaxNode GetUpdatedSyntax(SyntaxNode syntax, Diagnostic issue)
+        {
+            if (syntax is AssignmentExpressionSyntax expression)
+            {
+                var spaces = GetProposedSpaces(issue);
+
+                return expression.WithoutLeadingTrivia().WithLeadingSpaces(spaces);
+            }
+
+            return syntax;
+        }
+
+        private static SyntaxNode GetUpdatedSyntaxRoot(SyntaxNode root, SyntaxNode syntax)
+        {
+            if (syntax is AssignmentExpressionSyntax a && a.Parent is InitializerExpressionSyntax initializer)
+            {
+                var openBraceToken = initializer.OpenBraceToken;
+
+                if (initializer.CloseBraceToken.IsOnSameLineAs(openBraceToken))
+                {
+                    var expressions = initializer.Expressions;
+                    var index = expressions.IndexOf(a);
+
+                    switch (index)
+                    {
+                        case -1: // should never happen as the syntax should be part of the initializer
+                            break;
+
+                        case 0:
+                        {
+                            // ensure that the open brace token has no trailing trivia
+                            if (openBraceToken.HasTrailingTrivia)
+                            {
+                                return root.ReplaceNode(initializer, initializer.WithOpenBraceToken(openBraceToken.WithoutTrailingTrivia()));
+                            }
+
+                            break;
+                        }
+
+                        default:
+                        {
+                            var separator = expressions.GetSeparator(index - 1);
+
+                            // ensure that the separator token has no trailing trivia
+                            if (separator.HasTrailingTrivia)
+                            {
+                                return root.ReplaceToken(separator, separator.WithoutTrailingTrivia());
+                            }
+
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return root;
+        }
+    }
+}
---
+++ MiKo.Analyzer.Tests/Rules/Spacing/MiKo_6069_ObjectInitializerExpressionsAreIndentedBesideOpenBraceAnalyzerTests.cs
@@ -0,0 +1,269 @@
+﻿using Microsoft.CodeAnalysis.CodeFixes;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+using NUnit.Framework;
+
+using TestHelper;
+
+//// ncrunch: rdi off
+namespace MiKoSolutions.Analyzers.Rules.Spacing
+{
+    [TestFixture]
+    public sealed class MiKo_6069_ObjectInitializerExpressionsAreIndentedBesideOpenBraceAnalyzerTests : CodeFixVerifier
+    {
+        [Test]
+        public void No_issue_is_reported_for_initializer_expression_on_same_line() => No_issue_is_reported_for(@"
+public class TestMe
+{
+    public int Number { get; set; }
+
+    public static Create() => new TestMe { Number = 42 };
+}
+");
+
+        [Test]
+        public void No_issue_is_reported_for_initializer_expression_on_other_line_but_indented() => No_issue_is_reported_for(@"
+public class TestMe
+{
+    public int Number { get; set; }
+
+    public static Create() => new TestMe
+                                  {
+                                      Number = 42,
+                                  };
+}
+");
+
+        [Test]
+        public void An_issue_is_reported_for_initializer_expression_on_other_line_but_more_indented() => An_issue_is_reported_for(@"
+public class TestMe
+{
+    public int Number { get; set; }
+
+    public static Create() => new TestMe
+                                  {
+                                          Number = 42,
+                                  };
+}
+");
+
+        [Test]
+        public void An_issue_is_reported_for_initializer_expression_on_other_line_but_outdented() => An_issue_is_reported_for(@"
+public class TestMe
+{
+    public int Number { get; set; }
+
+    public static Create() => new TestMe
+                                  {
+                                  Number = 42,
+                                  };
+}
+");
+
+        [Test]
+        public void Code_gets_fixed_for_initializer_expression_on_other_line_but_outdented()
+        {
+            const string OriginalCode = @"
+public class TestMe
+{
+    public int Number { get; set; }
+
+    public static Create() => new TestMe
+                                  {
+                                          Number = 42,
+                                  };
+}
+";
+
+            const string FixedCode = @"
+public class TestMe
+{
+    public int Number { get; set; }
+
+    public static Create() => new TestMe
+                                  {
+                                      Number = 42,
+                                  };
+}
+";
+
+            VerifyCSharpFix(OriginalCode, FixedCode);
+        }
+
+        [Test]
+        public void Code_gets_fixed_for_initializer_expression_on_other_line_but_on_same_position_as_open_brace()
+        {
+            const string OriginalCode = @"
+public class TestMe
+{
+    public int Number { get; set; }
+
+    public static Create() => new TestMe
+                                  {
+                                  Number = 42,
+                                  };
+}
+";
+
+            const string FixedCode = @"
+public class TestMe
+{
+    public int Number { get; set; }
+
+    public static Create() => new TestMe
+                                  {
+                                      Number = 42,
+                                  };
+}
+";
+
+            VerifyCSharpFix(OriginalCode, FixedCode);
+        }
+
+        [Test]
+        public void Code_gets_fixed_for_initializer_expressions_on_other_lines()
+        {
+            const string OriginalCode = @"
+public class TestMe
+{
+    public int Name { get; set; }
+
+    public int Number { get; set; }
+
+    public static Create() => new TestMe
+                                  {
+                                        Name = ""my name"",
+                                  Number = 42,
+                                  };
+}
+";
+
+            const string FixedCode = @"
+public class TestMe
+{
+    public int Name { get; set; }
+
+    public int Number { get; set; }
+
+    public static Create() => new TestMe
+                                  {
+                                      Name = ""my name"",
+                                      Number = 42,
+                                  };
+}
+";
+
+            VerifyCSharpFix(OriginalCode, FixedCode);
+        }
+
+        [Test]
+        public void Code_gets_fixed_for_initializer_expression_on_same_line_but_on_position_0_character_behind_open_brace()
+        {
+            const string OriginalCode = @"
+public class TestMe
+{
+    public int Number { get; set; }
+
+    public static Create() => new TestMe {Number = 42 };
+}
+";
+
+            const string FixedCode = @"
+public class TestMe
+{
+    public int Number { get; set; }
+
+    public static Create() => new TestMe { Number = 42 };
+}
+";
+
+            VerifyCSharpFix(OriginalCode, FixedCode);
+        }
+
+        [Test]
+        public void Code_gets_fixed_for_initializer_expression_on_same_line_but_on_position_more_than_1_character_behind_open_brace()
+        {
+            const string OriginalCode = @"
+public class TestMe
+{
+    public int Number { get; set; }
+
+    public static Create() => new TestMe {   Number = 42 };
+}
+";
+
+            const string FixedCode = @"
+public class TestMe
+{
+    public int Number { get; set; }
+
+    public static Create() => new TestMe { Number = 42 };
+}
+";
+
+            VerifyCSharpFix(OriginalCode, FixedCode);
+        }
+
+        [Test]
+        public void Code_gets_fixed_for_initializer_expressions_on_same_line_but_with_additional_spaces()
+        {
+            const string OriginalCode = @"
+public class TestMe
+{
+    public int Name { get; set; }
+
+    public int Number { get; set; }
+
+    public static Create() => new TestMe {   Number = 42,      Name = ""some name"" };
+}
+";
+
+            const string FixedCode = @"
+public class TestMe
+{
+    public int Name { get; set; }
+
+    public int Number { get; set; }
+
+    public static Create() => new TestMe { Number = 42, Name = ""some name"" };
+}
+";
+
+            VerifyCSharpFix(OriginalCode, FixedCode);
+        }
+
+        [Test]
+        public void Code_gets_fixed_for_initializer_expressions_on_same_line_but_with_no_spaces()
+        {
+            const string OriginalCode = @"
+public class TestMe
+{
+    public int Name { get; set; }
+
+    public int Number { get; set; }
+
+    public static Create() => new TestMe {Number = 42,Name = ""some name"" };
+}
+";
+
+            const string FixedCode = @"
+public class TestMe
+{
+    public int Name { get; set; }
+
+    public int Number { get; set; }
+
+    public static Create() => new TestMe { Number = 42, Name = ""some name"" };
+}
+";
+
+            VerifyCSharpFix(OriginalCode, FixedCode);
+        }
+
+        protected override string GetDiagnosticId() => MiKo_6069_ObjectInitializerExpressionsAreIndentedBesideOpenBraceAnalyzer.Id;
+
+        protected override DiagnosticAnalyzer GetObjectUnderTest() => new MiKo_6069_ObjectInitializerExpressionsAreIndentedBesideOpenBraceAnalyzer();
+
+        protected override CodeFixProvider GetCSharpCodeFixProvider() => new MiKo_6069_CodeFixProvider();
+    }
+}
