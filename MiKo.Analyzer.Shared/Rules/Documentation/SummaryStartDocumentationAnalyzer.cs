50bbe20582bad1f8e5dd909f1cf26192a8caea50
Ralf Koban <r.kn@gmx.net>
2026-08-03 14:14:59 +0000

    Refactored 'AnalyzeTextStart' into some helper methods

--- MiKo.Analyzer.Shared/Rules/Documentation/SummaryStartDocumentationAnalyzer.cs
+++ MiKo.Analyzer.Shared/Rules/Documentation/SummaryStartDocumentationAnalyzer.cs
@@ -14,6 +14,12 @@ namespace MiKoSolutions.Analyzers.Rules.Documentation
         {
         }
 
+        private enum AnalysisResult
+        {
+            Completed = 0,
+            Continue = 1,
+        }
+
         protected virtual Diagnostic NonTextStartIssue(ISymbol symbol, SyntaxNode node) => StartIssue(symbol, node.GetLocation());
 
         protected virtual Diagnostic TextStartIssue(ISymbol symbol, Location location) => StartIssue(symbol, location);
@@ -53,96 +59,44 @@ namespace MiKoSolutions.Analyzers.Rules.Documentation
         {
             var tag = xml.StartTag.GetName();
 
-            var descendantNodes = xml.DescendantNodes();
-
-            foreach (var node in descendantNodes)
+            foreach (var node in xml.DescendantNodes())
             {
                 switch (node)
                 {
                     case XmlElementStartTagSyntax startTag:
                     {
-                        var tagName = startTag.GetName();
-
-                        if (tagName == tag || tagName is Constants.XmlTag.Para)
+                        if (AnalyzeStartTag(symbol, tag, startTag, out var issue) is AnalysisResult.Completed)
                         {
-                            continue; // skip over the start tag and name syntax
+                            return issue;
                         }
 
-                        return NonTextStartIssue(symbol, node); // it's no text, so it must be something different
+                        continue;
                     }
 
                     case XmlElementEndTagSyntax endTag:
                     {
-                        var tagName = endTag.GetName();
-
-                        if (tagName is Constants.XmlTag.Para)
+                        if (AnalyzeEndTag(symbol, endTag, out var issue) is AnalysisResult.Completed)
                         {
-                            continue; // skip over the start tag and name syntax
+                            return issue;
                         }
 
-                        if (endTag.Parent is XmlElementSyntax element)
-                        {
-                            if (ConsiderEmptyTextAsIssue(symbol))
-                            {
-                                return TextStartIssue(symbol, element.GetContentsLocation()); // it's an empty text
-                            }
-
-                            continue;
-                        }
-
-                        return NonTextStartIssue(symbol, node); // it's no text, so it must be something different
+                        continue;
                     }
 
                     case XmlNameSyntax _:
                     case XmlElementSyntax e when e.GetName() is Constants.XmlTag.Para:
                     case XmlEmptyElementSyntax ee when ee.GetName() is Constants.XmlTag.Para:
+                    {
                         continue; // skip over the start tag and name syntax
+                    }
 
                     case XmlTextSyntax text:
                     {
-                        // report the location of the first word(s) via the corresponding text token
-                        var textTokens = text.TextTokens;
-
-                        for (int index = 0, textTokensCount = textTokens.Count; index < textTokensCount; index++)
+                        if (AnalyzeText(symbol, text, out var issue) is AnalysisResult.Completed)
                         {
-                            var token = textTokens[index];
-
-                            if (token.IsKind(SyntaxKind.XmlTextLiteralNewLineToken))
-                            {
-                                continue;
-                            }
-
-                            var valueText = token.ValueText;
-
-                            if (valueText.IsNullOrWhiteSpace())
-                            {
-                                // we found the first but empty /// line, so ignore it
-                                continue;
-                            }
-
-                            if (valueText.Length is 1 && Constants.Comments.Delimiters.Contains(valueText[0]))
-                            {
-                                // this is a dot or something directly after the XML tag, so ignore that
-                                continue;
-                            }
-
-                            // we found some text
-                            if (AnalyzeTextStart(symbol, valueText, out var problematicText, out var comparison))
-                            {
-                                // it's no valid text, so we have an issue
-                                var start = valueText.IndexOf(problematicText, comparison);
-                                var end = start + problematicText.Length; // find end position for underlining
-
-                                var location = token.GetLocationWithOffset(start, end);
-
-                                return TextStartIssue(symbol, location);
-                            }
-
-                            // it's a valid text, so we quit
-                            return null;
+                            return issue;
                         }
 
-                        // we found a completely empty /// line, so ignore it
                         continue;
                     }
 
@@ -153,6 +107,111 @@ namespace MiKoSolutions.Analyzers.Rules.Documentation
 
             // nothing to report
             return null;
+        }
+
+        private AnalysisResult AnalyzeStartTag(ISymbol symbol, string tag, XmlElementStartTagSyntax startTag, out Diagnostic issue)
+        {
+            var tagName = startTag.GetName();
+
+            if (tagName == tag || tagName is Constants.XmlTag.Para)
+            {
+                // skip over the start tag and name syntax
+                issue = null;
+
+                return AnalysisResult.Continue;
+            }
+
+            // it's no text, so it must be something different
+            issue = NonTextStartIssue(symbol, startTag);
+
+            return AnalysisResult.Completed;
+        }
+
+        private AnalysisResult AnalyzeEndTag(ISymbol symbol, XmlElementEndTagSyntax endTag, out Diagnostic issue)
+        {
+            var tagName = endTag.GetName();
+
+            if (tagName is Constants.XmlTag.Para)
+            {
+                // skip over the start tag and name syntax
+                issue = null;
+
+                return AnalysisResult.Continue;
+            }
+
+            if (endTag.Parent is XmlElementSyntax element)
+            {
+                if (ConsiderEmptyTextAsIssue(symbol))
+                {
+                    // it's an empty text
+                    issue = TextStartIssue(symbol, element.GetContentsLocation());
+
+                    return AnalysisResult.Completed;
+                }
+
+                issue = null;
+
+                return AnalysisResult.Continue;
+            }
+
+            // it's no text, so it must be something different
+            issue = NonTextStartIssue(symbol, endTag);
+
+            return AnalysisResult.Completed;
+        }
+
+        private AnalysisResult AnalyzeText(ISymbol symbol, XmlTextSyntax text, out Diagnostic issue)
+        {
+            // report the location of the first word(s) via the corresponding text token
+            var textTokens = text.TextTokens;
+
+            for (int index = 0, textTokensCount = textTokens.Count; index < textTokensCount; index++)
+            {
+                var token = textTokens[index];
+
+                if (token.IsKind(SyntaxKind.XmlTextLiteralNewLineToken))
+                {
+                    continue;
+                }
+
+                var valueText = token.ValueText;
+
+                if (valueText.IsNullOrWhiteSpace())
+                {
+                    // we found the first but empty /// line, so ignore it
+                    continue;
+                }
+
+                if (valueText.Length is 1 && Constants.Comments.Delimiters.Contains(valueText[0]))
+                {
+                    // this is a dot or something directly after the XML tag, so ignore that
+                    continue;
+                }
+
+                // we found some text
+                if (AnalyzeTextStart(symbol, valueText, out var problematicText, out var comparison))
+                {
+                    // it's no valid text, so we have an issue
+                    var start = valueText.IndexOf(problematicText, comparison);
+                    var end = start + problematicText.Length; // find end position for underlining
+
+                    var location = token.GetLocationWithOffset(start, end);
+
+                    issue = TextStartIssue(symbol, location);
+
+                    return AnalysisResult.Completed;
+                }
+
+                // it's a valid text, so we quit
+                issue = null;
+
+                return AnalysisResult.Completed;
+            }
+
+            // we found a completely empty /// line, so ignore it
+            issue = null;
+
+            return AnalysisResult.Continue;
         }
     }
 }
