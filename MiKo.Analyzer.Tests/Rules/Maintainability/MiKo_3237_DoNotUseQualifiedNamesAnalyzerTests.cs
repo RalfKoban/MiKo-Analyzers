3e7ffb18aec2ea49b1d37ed3632ded3c56db2964
Ralf Koban <r.kn@gmx.net>
2026-08-04 17:19:18 +0000

    Added checks for specific scenarios to avoid costly type symbol lookups

--- MiKo.Analyzer.Tests/Rules/Maintainability/MiKo_3237_DoNotUseQualifiedNamesAnalyzerTests.cs
+++ MiKo.Analyzer.Tests/Rules/Maintainability/MiKo_3237_DoNotUseQualifiedNamesAnalyzerTests.cs
@@ -24,6 +24,171 @@ namespace Bla
 }
 ");
 
+        [Test]
+        public void No_issue_is_reported_for_binary_expression_with_no_fully_qualified_name() => No_issue_is_reported_for(@"
+using System;
+using System.IO;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public void DoSomething()
+        {
+            if (File.ReadAllText(""some path"") == ""MyName"")
+            {
+            }
+        }
+    }
+}
+");
+
+        [Test]
+        public void No_issue_is_reported_for_is_pattern_with_no_fully_qualified_name() => No_issue_is_reported_for(@"
+using System;
+using System.IO;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public void DoSomething()
+        {
+            if (File.ReadAllText(""some path"") is ""MyName"")
+            {
+            }
+        }
+    }
+}
+");
+
+        [Test]
+        public void No_issue_is_reported_for_binary_expression() => No_issue_is_reported_for(@"
+using System;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public string Name { get; set; }
+
+        public void DoSomething(TestMe other)
+        {
+            if (other.Name == ""MyName"")
+            {
+            }
+        }
+    }
+}
+");
+
+        [Test]
+        public void No_issue_is_reported_for_prefix_unary_expression() => No_issue_is_reported_for(@"
+using System;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public bool IsValid { get; set; }
+
+        public void DoSomething(TestMe other)
+        {
+            if (!other.IsValid)
+            {
+            }
+        }
+    }
+}
+");
+
+        [Test]
+        public void No_issue_is_reported_for_postfix_unary_expression() => No_issue_is_reported_for(@"
+using System;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public int Counter { get; set; }
+
+        public void DoSomething(TestMe other)
+        {
+            other.Counter++;
+        }
+    }
+}
+");
+
+        [Test]
+        public void No_issue_is_reported_for_assignment_expression() => No_issue_is_reported_for(@"
+using System;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public int Counter { get; set; }
+
+        public void DoSomething(TestMe other)
+        {
+            other.Counter += 1;
+        }
+    }
+}
+");
+
+        [Test]
+        public void No_issue_is_reported_for_is_pattern_with_literal() => No_issue_is_reported_for(@"
+using System;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public string Name { get; set; }
+
+        public void DoSomething(TestMe other)
+        {
+            if (other.Name is ""MyName"")
+            {
+            }
+        }
+    }
+}
+");
+
+        [Test]
+        public void No_issue_is_reported_for_parenthesized_expression_with_no_fully_qualified_name() => No_issue_is_reported_for(@"
+using System;
+using System.IO;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public void DoSomething()
+        {
+            var text = (File.ReadAllText(""some path""));
+        }
+    }
+}
+");
+
+        [Test]
+        public void No_issue_is_reported_for_conditional_expression_with_no_fully_qualified_name() => No_issue_is_reported_for(@"
+using System;
+using System.IO;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public string DoSomething(bool flag) => flag ? File.ReadAllText(""some path"") : string.Empty;
+    }
+}
+");
+
         [TestCase("System")]
         [TestCase("System.Collections")]
         [TestCase("System.Collections.Generic")]
@@ -74,6 +239,71 @@ namespace Bla
     public class TestMe
     {
         public object DoSomething() => System.Collections.Generic.KeyValuePair.Create(""a"", ""b"");
+    }
+}
+");
+
+        [Test]
+        public void An_issue_is_reported_for_binary_expression_with_fully_qualified_name_with_2_namespaces() => An_issue_is_reported_for(@"
+using System;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public void DoSomething(TestMe other)
+        {
+            if (System.IO.File.ReadAllText(""some path"") == ""MyName"")
+            {
+            }
+        }
+    }
+}
+");
+
+        [Test]
+        public void An_issue_is_reported_for_is_pattern_with_fully_qualified_name_with_2_namespaces() => An_issue_is_reported_for(@"
+using System;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public void DoSomething(TestMe other)
+        {
+            if (System.IO.File.ReadAllText(""some path"") is ""MyName"")
+            {
+            }
+        }
+    }
+}
+");
+
+        [Test]
+        public void An_issue_is_reported_for_parenthesized_expression_with__fully_qualified_name_with_2_namespaces() => An_issue_is_reported_for(@"
+using System;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public void DoSomething()
+        {
+            var text = (System.IO.File.ReadAllText(""some path""));
+        }
+    }
+}
+");
+
+        [Test]
+        public void An_issue_is_reported_for_conditional_expression_with_fully_qualified_name_with_2_namespaces() => An_issue_is_reported_for(@"
+using System;
+
+namespace Bla
+{
+    public class TestMe
+    {
+        public string DoSomething(bool flag) => flag ? System.IO.File.ReadAllText(""some path"") : string.Empty;
     }
 }
 ");
