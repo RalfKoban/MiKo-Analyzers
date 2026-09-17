3d74517724f9edaf333e39f1dda12aa1d82b77b5
Ralf Koban <r.kn@gmx.net>
2026-09-17 16:03:34 +0000

    Add new rule MiKo_1541 about 'actual' as prefix of parameters

---
+++ MiKo.Analyzer.Shared/Rules/Naming/MiKo_1541_ActualParameterAnalyzer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace MiKoSolutions.Analyzers.Rules.Naming
+{
+    [DiagnosticAnalyzer(LanguageNames.CSharp)]
+    public sealed class MiKo_1541_ActualParameterAnalyzer : NamingAnalyzer
+    {
+        public const string Id = "MiKo_1541";
+
+        private const string Prefix = "actual";
+
+        public MiKo_1541_ActualParameterAnalyzer() : base(Id, SymbolKind.Parameter)
+        {
+        }
+
+        protected override IEnumerable<Diagnostic> AnalyzeName(IParameterSymbol symbol, Compilation compilation)
+        {
+            var symbolName = symbol.Name;
+
+            if (symbolName.StartsWith(Prefix, StringComparison.Ordinal))
+            {
+                var betterName = FindBetterName(symbolName);
+
+                if (symbolName != betterName)
+                {
+                    return new[] { Issue(symbol, betterName, CreateBetterNameProposal(betterName)) };
+                }
+            }
+
+            return Array.Empty<Diagnostic>();
+        }
+
+        private static string FindBetterName(string name) => name.Length > Prefix.Length
+                                                             ? name.AsSpan(Prefix.Length).ToLowerCaseAt(0)
+                                                             : name;
+    }
+}
